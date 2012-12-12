6646dd0f9876098a37732be690e2a01dc098be53
Guewen Baconnier @ Camptocamp <guewen.baconnier@camptocamp.com>
2012-12-12 08:10:56 +0100

    [IMP] __openerp__ manifest: description to rst, set application, change name of the module

--- account_financial_report_webkit/__openerp__.py
+++ account_financial_report_webkit/__openerp__.py
@@ -18,12 +18,13 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-# TODO refactor helper in order to act more like mixin
-# By using properties we will have a more simple signature in fuctions
 {
-    'name': 'Webkit based extended report financial report',
+    'name': 'Financial Reports - Webkit',
     'description': """
-This module replace the following standard openerp financial reports :
+Financial Reports - Webkit
+==========================
+
+This module replaces or adds the following standard OpenERP financial reports:
  - General ledger
  - Trial Balance (simple or comparative view)
  - Partner ledger
@@ -32,50 +33,63 @@ This module replace the following standard openerp financial reports :
 
 
 Main improvements per report:
+-----------------------------
 
- * The General ledger: details of all entries posted in your books sorted by account.
-  - Filter by account in the wizard (no need to go to the Chart of account to do this anymore)
-    or by view account (the report will display all regular children account) ie: you can select all P&L accounts.
-  - The report will now print only account with movements OR with a balance not null. No more endless
-    report with accounts with no data. (field: display account is hidden)
-  - initial balance calculation on the fly if no open entry posted
-  - Thanks to a new checkbox in account form, you will have possibility to centralize any accounts you like.
-    Ie: you do not want to see all entries posted under the account ‘VAT on sales’; you will only see aggregated amounts by periods.
-  - Counterpart account displayed for each transaction (3 accounts max.) to ease searching.
-  - Better ergonomy on the wizard: important information at the top, filters in the middle, options
-    at the bottom or separate tab, more specific filtering on a other tabs. No more unique wizard layout
-    for all financial reports (ie: we have removed the journal tab for the GL report)
-  - improved report style
+The General ledger: details of all entries posted in your books sorted by account.
 
- * The partner ledger: details of entries relative to payable & receivable accounts posted in your books sorted by account and partner.
-  - Filter by partner now possible
-  - Now you can see accounts then Partner with subtotals for each account allowing you to check you data
-    with trial balance and partner balance for instance & accounts are ordered the same way than in the Chart of account
-  - period have been added (date only is uncompleted since date can be outside period)
-  - Reconciliation code added
-  - subtotal by account
-  - alpha sorting (same in partner balance)
+* Filter by account in the wizard (no need to go to the Chart of account to do this anymore)
+  or by view account (the report will display all regular children account) ie: you can select all P&L 
+  accounts.
+* The report will now print only account with movements OR with a balance not null. No more endless
+  report with accounts with no data. (field: display account is hidden)
+* initial balance calculation on the fly if no open entry posted
+* Thanks to a new checkbox in account form, you will have possibility to centralize any accounts you like.
+  Ie: you do not want to see all entries posted under the account ‘VAT on sales’; you will only see 
+  aggregated amounts by periods.
+* Counterpart account displayed for each transaction (3 accounts max.) to ease searching.
+* Better ergonomy on the wizard: important information at the top, filters in the middle, options
+  at the bottom or separate tab, more specific filtering on a other tabs. No more unique wizard layout
+  for all financial reports (ie: we have removed the journal tab for the GL report)
+* improved report style
 
- * Open invoice report : other version of the partner ledger showing unreconciled / partially reconcies entries
-(added on the 20/01/2012)
-  - Possibility to print unreconciled transactions only at any date in the past (thanks to the brand-new field:
-    last_rec_date which calculated the last move line reconciled date). No more pain to get open invoices at the last closing date.
-  - no initial balance calculated because the report shows open invoices from previous years.
+The partner ledger: details of entries relative to payable & receivable accounts posted in your books
+sorted by account and partner.
 
- * The Trial balance: list of account with balances
-  - you can either see the column : Initial balance , debit, credit , end balance or compare balances over 4 periods of your choice
-  - You can select the filter opening to get the opening trial balance only
-  - If you create a extra virtual charts (using consolidated account) of account for your P&L and your balance sheet,
-    you can print your statutory accounts (with comparision over years for ex.)
-  - If you compare 2 periods, you will get differences in value and % also
+* Filter by partner now possible
+* Now you can see accounts then Partner with subtotals for each account allowing you to check you data
+  with trial balance and partner balance for instance & accounts are ordered the same way than in the 
+  Chart of account
+* period have been added (date only is uncompleted since date can be outside period)
+* Reconciliation code added
+* subtotal by account
+* alpha sorting (same in partner balance)
 
- * The Partner balance: list of account with balances
-  - subtotal by account & partner
-  - alpha sorting (same in partner balance)
+Open invoice report : other version of the partner ledger showing unreconciled / partially reconcies entries
+
+* Possibility to print unreconciled transactions only at any date in the past (thanks to the brand-new field:
+  `last_rec_date` which calculated the last move line reconciled date). No more pain to get open invoices at 
+  the last closing date.
+* no initial balance calculated because the report shows open invoices from previous years.
+
+The Trial balance: list of account with balances
+
+* you can either see the column : Initial balance , debit, credit , end balance or compare balances
+  over 4 periods of your choice
+* You can select the filter opening to get the opening trial balance only
+* If you create a extra virtual charts (using consolidated account) of account for your P&L 
+  and your balance sheet, you can print your statutory accounts (with comparision over years for ex.)
+* If you compare 2 periods, you will get differences in value and % also
+
+The Partner balance: list of account with balances
+
+* subtotal by account & partner
+* alpha sorting (same in partner balance)
 
 Limitations:
-In order to run properly this module make sure you have installed the librairie ‘wkhtmltopdf’
-for the pdf rendering (this library path must be added to you company settings).
+------------
+
+In order to run properly this module make sure you have installed the library `wkhtmltopdf`
+for the pdf rendering (the library path must be set in a System Parameter `webkit_path`).
 
 Initial balances in these reports are based either on opening entry
 posted in the opening period or calculated on the fly. So make sure,
@@ -86,15 +100,14 @@ date Vs IB by period) The opening period is assumed to be the 01.01 of
 the year with an opening flag and the first period of the year must
 starts also the 01.01
 
-Totals for amount in currencies are affective if the partner belong to
+Totals for amount in currencies are effective if the partner belongs to
 an account with a secondary currency.
 
 html headers and footers are deactivated for these reports because of
-an issue of wkhtmltopdf :
-http://code.google.com/p/wkhtmltopdf/issues/detail?id=656 Instead, the
-header and footer are created as text with arguments passed to
+an issue in wkhtmltopdf:
+http://code.google.com/p/wkhtmltopdf/issues/detail?id=656
+Instead, the header and footer are created as text with arguments passed to
 wkhtmltopdf. The texts are defined inside the report classes.
-
 """,
     'version': '1.0',
     'author': 'Camptocamp',
@@ -127,4 +140,5 @@ wkhtmltopdf. The texts are defined inside the report classes.
     #'tests/account_move_line.yml'
     'active': False,
     'installable': True,
+    'application': True,
 }
