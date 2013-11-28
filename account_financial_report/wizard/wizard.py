8ed3576105cb6136e290c7eac021daf435dd9780
Humberto Arocha <humbertoarocha@gmail.com>
2013-11-27 19:10:59 -0530

    [AUTOPEP8][account_financial_report] autopep8 account_financial_report --recursive --in-place --pep8-passes 2000 --verbose

--- account_financial_report/wizard/wizard.py
+++ account_financial_report/wizard/wizard.py
@@ -51,16 +51,16 @@ class wizard_report(osv.osv_memory):
 
         'analytic_ledger': fields.boolean('Analytic Ledger', help="Allows to Generate an Analytic Ledger for accounts with moves. Available when Balance Sheet and 'Initial | Debit | Credit | YTD' are selected"),
         'journal_ledger': fields.boolean('Journal Ledger', help="Allows to Generate an Journal Ledger for accounts with moves. Available when Balance Sheet and 'Initial | Debit | Credit | YTD' are selected"),
-        'partner_balance': fields.boolean('Partner Balance', help="""Allows to
-            Generate a Partner Balance for accounts with moves. Available when
-            Balance Sheet and 'Initial | Debit | Credit | YTD' are selected"""),
+        'partner_balance': fields.boolean('Partner Balance', help="Allows to "
+                                          "Generate a Partner Balance for accounts with moves. Available when "
+                                          "Balance Sheet and 'Initial | Debit | Credit | YTD' are selected"),
         'tot_check': fields.boolean('Summarize?', help='Checking will add a new line at the end of the Report which will Summarize Columns in Report'),
         'lab_str': fields.char('Description', help='Description for the Summary', size=128),
 
         'target_move': fields.selection([('posted', 'All Posted Entries'),
                                         ('all', 'All Entries'),
                                          ], 'Entries to Include', required=True,
-                                         help='Print All Accounting Entries or just Posted Accounting Entries'),
+                                        help='Print All Accounting Entries or just Posted Accounting Entries'),
         #~ Deprecated fields
         'filter': fields.selection([('bydate', 'By Date'), ('byperiod', 'By Period'), ('all', 'By Date and Period'), ('none', 'No Filter')], 'Date/Period Filter'),
         'date_to': fields.date('End date'),
