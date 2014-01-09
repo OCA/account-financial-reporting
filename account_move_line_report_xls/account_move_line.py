9c497c8e26c93c9b7e3c89de8a4c3343a764582c
Luc De Meyer <luc.demeyer@noviat.com>
2014-01-09 12:07:08 +0100

    update account_move_line_report_xls

--- account_move_line_report_xls/account_move_line.py
+++ account_move_line_report_xls/account_move_line.py
@@ -3,7 +3,7 @@
 #
 #    OpenERP, Open Source Management Solution
 #
-#    Copyright (c) 2013 Noviat nv/sa (www.noviat.com). All rights reserved.
+#    Copyright (c) 2014 Noviat nv/sa (www.noviat.com). All rights reserved.
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -12,11 +12,11 @@
 #
 #    This program is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 #    GNU Affero General Public License for more details.
 #
 #    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
 
@@ -24,22 +24,28 @@ from openerp.osv import orm
 from openerp.addons.report_xls.utils import rowcol_to_cell, _render
 from openerp.tools.translate import _
 
+
 class account_move_line(orm.Model):
     _inherit = 'account.move.line'
-    
+
     # override list in custom module to add/drop columns or change order
     def _report_xls_fields(self, cr, uid, context=None):
         return [
-            'move', 'name', 'date', 'period', 'partner', 'account',
-            'date_maturity', 'debit', 'credit', 'balance', 'reconcile', 'reconcile_partial',
-            #'amount_currency', 'currency_name',
+            'move', 'name', 'date', 'journal', 'period', 'partner', 'account',
+            'date_maturity', 'debit', 'credit', 'balance',
+            'reconcile', 'reconcile_partial', 'analytic_account',
+            #'ref', 'partner_ref', 'tax_code', 'tax_amount', 'amount_residual',
+            #'amount_currency', 'currency_name', 'company_currency',
+            #'amount_residual_currency',
+            #'product', 'product_ref', 'product_uom', 'quantity',
+            #'statement', 'invoice', 'narration', 'blocked',
         ]
-        
+
     # Change/Add Template entries
-    def _report_xls_template(self, cr, uid, context=None):   
-        """  
-        Template updates, e.g. 
-        
+    def _report_xls_template(self, cr, uid, context=None):
+        """
+        Template updates, e.g.
+
         my_change = {
             'move':{
                 'header': [1, 20, 'text', _('My Move Title')],
@@ -47,6 +53,5 @@ class account_move_line(orm.Model):
                 'totals': [1, 0, 'text', None]},
         }
         return my_change
-        """   
+        """
         return {}
-
