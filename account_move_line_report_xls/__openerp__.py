80210463feb4e9a8cb7023c4c78fb3d9e71ce988
Luc De Meyer <luc.demeyer@noviat.com>
2014-01-09 12:07:08 +0100

    update account_move_line_report_xls

--- account_move_line_report_xls/__openerp__.py
+++ account_move_line_report_xls/__openerp__.py
@@ -3,7 +3,7 @@
 #
 #    OpenERP, Open Source Management Solution
 #
-#    Copyright (c) 2013 Noviat nv/sa (www.noviat.com). All rights reserved.
+#    Copyright (c) 2014 Noviat nv/sa (www.noviat.com). All rights reserved.
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -12,35 +12,63 @@
 #
 #    This program is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 #    GNU Affero General Public License for more details.
 #
 #    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
 
 {
     'name': 'Account Move Line XLS export',
-    'version': '0.5',
+    'version': '0.6',
     'license': 'AGPL-3',
     'author': 'Noviat',
-    'category' : 'Accounting & Finance',
+    'category': 'Accounting & Finance',
     'description': """
-
 Journal Items Excel Export
 ==========================
 
-This module adds a button on the journal items ('account.move.line') list view in order to export the selected lines.
+This module adds a button on the journal items ('account.move.line') list view
+in order to export the selected lines.
 
-If you are installing this module manually, you need also the module 'report_xls', that is located in:
-https://launchpad.net/openerp-reporting-engines"
+If you are installing this module manually, you need also the **report_xls**
+module located in:
+https://launchpad.net/openerp-reporting-engines
 
+Report customisation
+''''''''''''''''''''
+
+The Excel export can be tailored to your exact needs via the following methods
+of the 'account.move.line' object:
+
+*  **_report_xls_fields**
+
+   Add/drop columns or change order from the list of columns that are defined
+   in the Excel template.
+
+   The following fields are defined in the Excel template:
+
+     move, name, date, journal, period, partner, account,
+     date_maturity, debit, credit, balance,
+     reconcile, reconcile_partial, analytic_account,
+     ref, partner_ref, tax_code, tax_amount, amount_residual,
+     amount_currency, currency_name, company_currency,
+     amount_residual_currency, product, product_ref', product_uom, quantity,
+     statement, invoice, narration, blocked
+
+* **_report_xls_template**
+
+  Change/extend the Excel template.
+
+Customisation assistance
+''''''''''''''''''''''''
+Contact info@noviat.com for help with the customisation and/or development
+of Excel reports in OpenERP.
     """,
     'depends': ['account', 'report_xls'],
-    'demo_xml': [],
-    'init_xml': [],
-    'update_xml' : [
-        'report/move_line_list_xls.xml',      
+    'data': [
+        'report/move_line_list_xls.xml',
     ],
 }
