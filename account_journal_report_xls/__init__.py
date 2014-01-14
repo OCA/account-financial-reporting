f320c8633fce4088f3094c4e078f720f034bb868
Luc De Meyer <luc.demeyer@noviat.com>
2014-01-14 20:31:12 +0100

    check report_xls

--- account_journal_report_xls/__init__.py
+++ account_journal_report_xls/__init__.py
@@ -20,8 +20,12 @@
 #
 ##############################################################################
 
-from . import account_journal
-from . import wizard
-from . import report
+try:
+    from . import account_journal
+    from . import wizard
+    from . import report
+except ImportError:
+    import logging
+    logging.getLogger('openerp.module').warning('report_xls not available in addons path. account_financial_report_webkit_xls will not be usable')
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
