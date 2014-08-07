4f92a87e0f17c65c335d6aa52f7035995da339ea
laetitia.gangloff@acsone.eu <laetitia.gangloff@acsone.eu>
2014-08-07 11:17:05 +0200

    [pep8] - account_export_csv

--- account_export_csv/wizard/__init__.py
+++ account_export_csv/wizard/__init__.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-#    Author Joel Grand-Guillaume and Vincent Renaville Copyright 2013 Camptocamp SA
+#    Author Joel Grand-Guillaume and Vincent Renaville Copyright 2013
+#    Camptocamp SA
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
