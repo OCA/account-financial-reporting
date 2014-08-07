ac9b62fc213aa9a58647174f07b46520e93a89fd
laetitia.gangloff@acsone.eu <laetitia.gangloff@acsone.eu>
2014-08-07 13:08:20 +0200

    [pep8] - account_financial_report_horizontal
    [pep8] - review on account_export_csv, account_move_line_report_xls, account_journal_report_xls, account_financial_report_horizontal, account_financial_report_webkit_xls
    
    [pep8] review on account_financial_report, account_financial_report_horizontal, account_financial_report_webkit_xls, account_journal_report_xls, account_move_line_report_xls

--- account_financial_report_horizontal/wizard/__init__.py
+++ account_financial_report_horizontal/wizard/__init__.py
@@ -1,4 +1,4 @@
-import account_report_common
-import account_report_common_account
-import account_report_balance_sheet
-import account_report_profit_loss
+from . import account_report_common
+from . import account_report_common_account
+from . import account_report_balance_sheet
+from . import account_report_profit_loss
