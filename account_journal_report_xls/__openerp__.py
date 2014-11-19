4a55f36be9902081808bdfabfbbdb111b03d6408
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2014-11-19 11:00:54 +0100

    Extract the description in README.rst

--- account_journal_report_xls/__openerp__.py
+++ account_journal_report_xls/__openerp__.py
@@ -26,23 +26,6 @@
     'license': 'AGPL-3',
     'author': 'Noviat',
     'category': 'Accounting & Finance',
-    'description': """
-
-Journal Reports
-===============
-
-This module adds journal reports by period and by fiscal year with
-    - entries printed per move
-    - option to group entries with same general account & VAT case
-    - vat info per entry
-    - vat summary
-
-These reports are available in PDF and XLS format.
-
-This module depends upon the 'report_xls' module,
-cf. https://github.com/OCA/reporting-engine
-
-    """,
     'depends': [
         'account_voucher',
         'report_xls',
