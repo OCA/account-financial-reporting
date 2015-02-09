10dfe75581189d891993eea9fb31306c2f5e8736
Alexis de Lattre <alexis@via.ecp.fr>
2015-02-09 23:15:13 +0100

    Continue port to new API + PEP8. First working version.

--- intrastat_base/__openerp__.py
+++ intrastat_base/__openerp__.py
@@ -2,7 +2,7 @@
 ##############################################################################
 #
 #    Report intrastat base module for Odoo
-#    Copyright (C) 2011-2014 Akretion (http://www.akretion.com)
+#    Copyright (C) 2011-2015 Akretion (http://www.akretion.com)
 #    @author Alexis de Lattre <alexis.delattre@akretion.com>
 #
 #    This program is free software: you can redistribute it and/or modify
@@ -19,7 +19,6 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-
 
 {
     'name': 'Intrastat Reporting Base',
--- intrastat_product/__openerp__.py
+++ intrastat_product/__openerp__.py
@@ -1,8 +1,8 @@
 # -*- encoding: utf-8 -*-
 ##############################################################################
 #
-#    Intrastat Product module for OpenERP
-#    Copyright (C) 2011-2014 Akretion (http://www.akretion.com)
+#    Intrastat Product module for Odoo
+#    Copyright (C) 2011-2015 Akretion (http://www.akretion.com)
 #    @author Alexis de Lattre <alexis.delattre@akretion.com>
 #
 #    This program is free software: you can redistribute it and/or modify
@@ -19,7 +19,6 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-
 
 {
     'name': 'Intrastat Product',
