9ba531bf39901085a78aa64c6d43711ba9c73056
fv-clearcorp <fabio.vilchez@clearcorp.co.cr>
2015-07-31 16:13:30 -0600

    [IMP] - account_financial_report_webkit_xls: Added new reports to module.

---
+++ account_financial_report_webkit_xls/wizard/account_financial_wizard.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Addons modules by CLEARCORP S.A.
+#    Copyright (C) 2009-TODAY CLEARCORP S.A. (<http://clearcorp.co.cr>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp.osv import orm
+
+
+class account_financial_wizard(orm.TransientModel):
+
+    _inherit = 'accounting.report'
+
+    def xls_export(self, cr, uid, ids, context=None):
+        return self.check_report(cr, uid, ids, context=context)
+
+    def _print_report(self, cr, uid, ids, data, context=None):
+        context = context or {}
+        if context.get('xls_export'):
+            data['form'].update(
+                self.read(cr, uid, ids, [
+                    'date_from_cmp',  'debit_credit', 'date_to_cmp',
+                    'fiscalyear_id_cmp', 'period_from_cmp', 'period_to_cmp',
+                    'filter_cmp', 'account_report_id', 'enable_filter',
+                    'label_filter', 'target_move'], context=context)[0])
+            return {
+                'type': 'ir.actions.report.xml',
+                'report_name': 'account.report_financial_xls',
+                'datas': data,
+                'data': data}
+        else:
+            return super(account_financial_wizard, self)._print_report(
+                cr, uid, ids, data, context=context)
