af05e07136e759f918676181f5289891bdbff6e0
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2015-10-09 09:59:21 +0200

    [UPD] prefix versions with 8.0

--- account_chart_report/__openerp__.py
+++ account_chart_report/__openerp__.py
@@ -23,7 +23,7 @@
 
 {
     'name': 'Print chart of accounts',
-    'version': '1.0',
+    'version': '8.0.1.0.0',
     'category': 'Reports/pdf',
     'author': "Savoir-faire Linux,Odoo Community Association (OCA)",
     'website': 'http://www.savoirfairelinux.com',
--- account_financial_report_webkit_xls/__openerp__.py
+++ account_financial_report_webkit_xls/__openerp__.py
@@ -21,7 +21,7 @@
 ##############################################################################
 {
     'name': 'Add XLS export to accounting reports',
-    'version': '0.4',
+    'version': '8.0.0.4.0',
     'license': 'AGPL-3',
     'author': "Noviat,Odoo Community Association (OCA)",
     'category': 'Generic Modules/Accounting',
