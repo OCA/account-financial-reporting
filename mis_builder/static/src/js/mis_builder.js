d01cfa348d7fc32edeb70f3058811cf9083ea3e3
Adrien Peiffer (ACSONE) <adrien.peiffer@acsone.eu>
2015-12-17 09:31:27 +0100

    Not really the same js in 9.0 ...

--- mis_builder/static/src/js/mis_builder.js
+++ mis_builder/static/src/js/mis_builder.js
@@ -1,48 +1,59 @@
-openerp.mis_builder = function(instance) {
+odoo.define('mis.builder', function (require) {
+"use strict";
 
-    instance.mis_builder.MisReport = instance.web.form.FormWidget.extend({
-        template: "mis_builder.MisReport",
+var core = require('web.core');
+var form_common = require('web.form_common');
+var Model = require('web.DataModel');
+var data = require('web.data');
 
-        init: function() {
-            this._super.apply(this, arguments);
-            this.mis_report_data = null;
-        },
+var MisReport = form_common.FormWidget.extend({
+    /**
+     * @constructs instance.mis_builder.MisReport
+     * @extends instance.web.form.FormWidget
+     * 
+     */
+    template: "mis_builder.MisReport",
+    init: function() {
+        this._super.apply(this, arguments);
+        this.mis_report_data = null;
+    },
 
-        start: function() {
-            this._super.apply(this, arguments);
-            var self = this;
-            new instance.web.Model("mis.report.instance").call(
-                "compute", 
-                [self.getParent().dataset.context.active_id], 
-                {'context': new instance.web.CompoundContext()}
-            ).then(function(result){
-                self.mis_report_data = result;
-                self.renderElement();
+    start: function() {
+        this._super.apply(this, arguments);
+        var self = this;
+        new Model("mis.report.instance").call(
+            "compute", 
+            [self.getParent().dataset.context.active_id], 
+            {'context': new data.CompoundContext()}
+        ).then(function(result){
+            self.mis_report_data = result;
+            self.renderElement();
+        });
+    },
+
+    events: {
+        "click a.mis_builder_drilldown": "drilldown",
+    },
+
+    drilldown: function(event) {
+        var self = this;
+        var drilldown = JSON.parse($(event.target).data("drilldown"));
+        if (drilldown) {
+            var period_id = JSON.parse($(event.target).data("period-id"));
+            var val_c = JSON.parse($(event.target).data("expr"));
+            new Model("mis.report.instance.period").call(
+                "drilldown",
+                [period_id, val_c],
+                {'context': new data.CompoundContext()}
+            ).then(function(result) {
+                if (result) {
+                    self.do_action(result);
+                }
             });
-        },
+        }
+    },
+});
 
-        events: {
-            "click a.mis_builder_drilldown": "drilldown",
-        },
+core.form_custom_registry.add('mis_report', MisReport);
 
-        drilldown: function(event) {
-            var self = this;
-            var drilldown = JSON.parse($(event.target).data("drilldown"));
-            if (drilldown) {
-                var period_id = JSON.parse($(event.target).data("period-id"));
-                var val_c = JSON.parse($(event.target).data("expr"));
-                new instance.web.Model("mis.report.instance.period").call(
-                    "drilldown",
-                    [period_id, val_c],
-                    {'context': new instance.web.CompoundContext()}
-                ).then(function(result) {
-                    if (result) {
-                        self.do_action(result);
-                    }
-                });
-            }
-        },
-    });
-
-    instance.web.form.custom_widgets.add('mis_report', 'instance.mis_builder.MisReport');
-}
+});
