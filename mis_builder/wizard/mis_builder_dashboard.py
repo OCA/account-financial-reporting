9f998c454fc32542adf2771f7897d2e22a312749
jbeficent <jordi.ballester@eficent.com>
2016-02-05 14:23:22 +0100

    [FIX] auto test errors, OCA style

--- mis_builder/wizard/mis_builder_dashboard.py
+++ mis_builder/wizard/mis_builder_dashboard.py
@@ -1,32 +1,12 @@
-# -*- encoding: utf-8 -*-
-##############################################################################
-#
-#    mis_builder module for Odoo, Management Information System Builder
-#    Copyright (C) 2014-2015 ACSONE SA/NV (<http://acsone.eu>)
-#
-#    This file is a part of mis_builder
-#
-#    mis_builder is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License v3 or later
-#    as published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    mis_builder is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License v3 or later for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    v3 or later along with this program.
-#    If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# -*- coding: utf-8 -*-
+# © 2014-2015 ACSONE SA/NV (<http://acsone.eu>)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from openerp.osv import orm, fields
 from lxml import etree
 
 
-class add_mis_report_instance_dashboard(orm.TransientModel):
+class AddMisReportInstanceDashboard(orm.TransientModel):
     _name = "add.mis.report.instance.dashboard.wizard"
 
     _columns = {'name': fields.char('Name', size=32, required=True),
@@ -39,8 +19,9 @@ class add_mis_report_instance_dashboard(orm.TransientModel):
     def default_get(self, cr, uid, fields, context=None):
         if context is None:
             context = {}
+        res = {}
         if context.get('active_id'):
-            res = super(add_mis_report_instance_dashboard, self).default_get(
+            res = super(AddMisReportInstanceDashboard, self).default_get(
                 cr, uid, fields, context=context)
             # get report instance name
             res['name'] = self.pool['mis.report.instance'].read(
