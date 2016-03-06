215009c1bb9c196a57c2e442b15b8aa9dcbf9949
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2016-03-06 14:26:48 +0100

    [ADD] mis_builder_analyic_filter

---
+++ mis_builder_analytic_filter/README.rst
@@ -0,0 +1,74 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+   :target: http://www.gnu.org/licenses/agpl-3.0-standalone.html
+   :alt: License: AGPL-3
+
+===========================
+MIS Builder Analytic Filter
+===========================
+
+This module extends the functionality of mis_builder to
+provide filtering on analytic axis.
+
+Installation
+============
+
+There is no specific installation procedure for this module.
+
+Configuration and Usage
+=======================
+
+In Accounting > Reporting > MIS Reports, there is a new field
+'Analytic Account' which can be used to filter move line on an
+analytic axis and it's children.
+
+This filter is also available on the MIS Report widget, so the user
+can change the filter in the preview and dashboard views.
+
+.. image:: https://odoo-community.org/website/image/ir.attachment/5784_f2813bd/datas
+   :alt: Try me on Runbot
+   :target: https://runbot.odoo-community.org/runbot/91/8.0
+
+Known issues / Roadmap
+======================
+
+While useful as is, we are aware that such analytic filtering may need
+to be customized heavily depending on the customer context. This module can
+be extended or considered as an example.
+
+* at the moment, no filtering is done on other queries than move lines
+
+Bug Tracker
+===========
+
+Bugs are tracked on `GitHub Issues
+<https://github.com/OCA/account-financial-reporting/issues>`_. In case of trouble, please
+check there if your issue has already been reported. If you spotted it first,
+help us smashing it by providing a detailed and welcomed `feedback
+<https://github.com/OCA/
+account-financial-reporting/issues/new?body=module:%20
+mis_builder_analytic_filter%0Aversion:%20
+8.0%0A%0A**Steps%20to%20reproduce**%0A-%20...%0A%0A**Current%20behavior**%0A%0A**Expected%20behavior**>`_.
+
+Credits
+=======
+
+Contributors
+------------
+
+* Adrien Peiffer <adrien.peiffer@acsone.eu>
+* Stéphane Bidoul <stephane.bidoul@acsone.eu>
+
+Maintainer
+----------
+
+.. image:: https://odoo-community.org/logo.png
+   :alt: Odoo Community Association
+   :target: https://odoo-community.org
+
+This module is maintained by the OCA.
+
+OCA, or the Odoo Community Association, is a nonprofit organization whose
+mission is to support the collaborative development of Odoo features and
+promote its widespread use.
+
+To contribute to this module, please visit https://odoo-community.org.
---
+++ mis_builder_analytic_filter/static/src/js/mis_builder.js
@@ -0,0 +1,72 @@
+openerp.mis_builder_analytic_filter = function(instance) {
+
+    instance.mis_builder.MisReport.include({
+
+        init: function() {
+            this._super.apply(this, arguments);
+            this.account_analytic_id = false;
+            this.initialized = false;
+            this.mis_report_instance_id = false;
+        },
+        initialize_field: function() {
+            var self = this;
+            self.destroy_content();
+            self.init_fields();
+        },
+        destroy_content: function() {
+            if (this.dfm) {
+                this.dfm.destroy();
+                this.dfm = undefined;
+            }
+        },
+        get_context: function() {
+            var self = this;
+            context = this._super.apply(this, arguments);
+            context['account_analytic_id'] = this.account_analytic_id;
+            return context
+        },
+        init_fields: function() {
+            var self = this;
+            if (self.dfm)
+                return;
+            self.$(".oe_mis_builder_analytic_axis").show();
+            self.dfm = new instance.web.form.DefaultFieldManager(self);
+            self.dfm.extend_field_desc({
+                account: {
+                    relation: "account.analytic.account",
+                },
+            });
+            self.account_m2o = new instance.web.form.FieldMany2One(self.dfm, {
+                attrs: {
+                    placeholder: _t("Analytic Account"),
+                    name: "account",
+                    type: "many2one",
+                    domain: [],
+                    context: {},
+                    modifiers: '{}',
+                },
+            });
+            if (this.initialized) {
+                self.account_m2o.set('value', this.account_analytic_id);
+            } else {
+                val = self.getParent().dataset.context.account_analytic_id
+                if (val) {
+                    self.account_m2o.set('value', val);
+                    this.account_analytic_id = val
+                }
+                this.initialized = true;
+            }
+            self.account_m2o.prependTo(self.$(".oe_mis_builder_analytic_axis"));
+            self.account_m2o.$input.focusout(function(){
+                self.account_analytic_id = self.account_m2o.get_value();
+            });
+            self.$(".oe_mis_builder_generate_content").click(_.bind(this.generate_content, this));
+        },
+        renderElement: function() {
+            this._super();
+            var self = this;
+            self.initialize_field();
+        },
+
+});
+}
