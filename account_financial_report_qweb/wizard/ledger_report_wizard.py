eeddf920cc84349fd407d2cfe8283783f474bdd6
Damien Crier <damien.crier@camptocamp.com>
2016-04-26 19:24:02 +0200

    [ADD] generic wizard for ledger

--- account_financial_report_qweb/wizard/ledger_report_wizard.py
+++ account_financial_report_qweb/wizard/ledger_report_wizard.py
@@ -2,7 +2,7 @@
 # Author: Damien Crier
 # Copyright 2016 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-from openerp import models, fields
+from openerp import models, fields, api
 
 
 class LedgerReportWizard(models.TransientModel):
@@ -11,3 +11,24 @@ class LedgerReportWizard(models.TransientModel):
     _description = "Ledger Report"
 
     company_id = fields.Many2one(comodel_name='res.company')
+    # date_range = ??
+    date_from = fields.Date()
+    date_to = fields.Date()
+    target_move = fields.Selection([('posted', 'All Posted Entries'),
+                                    ('all', 'All Entries')],
+                                   string='Target Moves',
+                                   required=True,
+                                   default='posted')
+    account_ids = fields.Many2many(
+        comodel_name='account.account',
+        relation='account_account_ledger_rel',
+        string='Filter accounts',
+    )
+    amount_currency = fields.Boolean(string='With currency',
+                                     default=False)
+    centralize = fields.Boolean(string='Activate centralization',
+                                default=False)
+
+    @api.multi
+    def check_report(self):
+        return True
