04ad9c1fee4b3294aaa1604e5db0fa041e5e5260
os-kng <k.graveman@onestein.nl>
2016-04-26 19:23:12 +0200

    [ADD] Wizard aged partner balance

---
+++ account_financial_report_qweb/wizard/aged_partner_balance_wizard.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# Author: Damien Crier
+# Copyright 2016 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import models, fields
+
+
+class AccountAgedTrialBalance(models.TransientModel):
+
+    _name = 'account.aged.trial.balance.qweb'
+    _description = 'Aged partner balanced'
+
+    # date_range_id = fields.Many2one(
+    #     'date.range',
+    #     string='Date Range')
+    company_id = fields.Many2one(
+        comodel_name='res.company',
+        string='Company')
+    date_from = fields.Date('Date from')
+    date_to = fields.Date('Date to')
+
+    partner_ids = fields.Many2many(
+        'res.partner',
+        string='Filter on partner',
+        help='Only selected partners will be printed. '
+        'Leave empty to print all partners.')
