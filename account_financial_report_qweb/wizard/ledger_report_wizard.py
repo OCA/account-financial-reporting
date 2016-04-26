7b224ee66d649d5f46024772ab6ef0ec13af37b6
Leonardo Pistone <leonardo.pistone@camptocamp.com>
2016-04-26 18:51:56 +0200

    remove unused imports

--- account_financial_report_qweb/wizard/ledger_report_wizard.py
+++ account_financial_report_qweb/wizard/ledger_report_wizard.py
@@ -2,9 +2,7 @@
 # Author: Damien Crier
 # Copyright 2016 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-import time
-
-from openerp import models, fields, api
+from openerp import models, fields
 
 
 class LedgerReportWizard(models.TransientModel):
