b8e34e8acdf3b235e78c40dcc618330cc5f11194
Damien Crier <damien.crier@camptocamp.com>
2016-04-26 17:32:02 +0200

    [ADD] new module to manage financial reports in OCA

---
+++ account_financial_report_qweb/__openerp__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# Author: Damien Crier
+# Copyright 2016 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    'name': 'QWeb Financial Reports',
+    'version': '9.0.0.1.0',
+    'category': 'Reporting',
+    'summary': """
+        OCA Financial Reports
+    """,
+    'author': 'Camptocamp SA,'
+              'Odoo Community Association (OCA)',
+    'website': 'http://www.camptocamp.com',
+    'depends': [
+        'account',
+    ],
+    'data': [
+    ],
+    'test': [
+    ],
+    'demo': [
+    ],
+    'qweb': [
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'license': 'AGPL-3',
+}
