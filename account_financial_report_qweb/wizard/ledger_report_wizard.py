18232a7261d2475c8e39e8f63c5513bc0b6202d0
Damien Crier <damien.crier@camptocamp.com>
2016-04-26 18:36:02 +0200

    [ADD] add base wizard for ledger

---
+++ account_financial_report_qweb/wizard/ledger_report_wizard.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Author: Damien Crier
+# Copyright 2016 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+import time
+
+from openerp import models, fields, api
+
+
+class LedgerReportWizard(models.TransientModel):
+
+    _name = "ledger.report.wizard"
+    _description = "Ledger Report"
+
+    company_id = fields.Many2one(comodel_name='res.company')
