b94cdd8bd416839feeab2d5c66639f17dc70c1d7
robert rottermann <robert@redcor.ch>
2016-04-27 18:28:03 +0200

    [ADD]added new wizzard account_financial_report_qweb/wizard/balance_sheet_wizard_view.xml

--- account_financial_report_qweb/__openerp__.py
+++ account_financial_report_qweb/__openerp__.py
@@ -25,6 +25,8 @@
         'wizard/balance_common_wizard_view.xml',
         'wizard/partner_balance_wizard_view.xml',
         'wizard/trial_balance_wizard_view.xml',
+        'wizard/balance_sheet_wizard_view.xml',
+        #'wizard/profit_loss_wizard_view.xml',
         'views/report_menus.xml',
         'menuitems.xml',
         'reports.xml',
