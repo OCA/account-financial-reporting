b560fa750e98e83e6b655bcfbfa7d79d3e3649b1
Thomas Rehn <thomas.rehn@initos.com>
2016-04-27 19:12:10 +0200

    amend author information

--- account_financial_report_qweb/__openerp__.py
+++ account_financial_report_qweb/__openerp__.py
@@ -11,6 +11,7 @@
     """,
     'author': 'Camptocamp SA,'
               'initOS GmbH,'
+              'redCOR AG',
               'Odoo Community Association (OCA)',
     'website': 'http://www.camptocamp.com',
     'depends': [
