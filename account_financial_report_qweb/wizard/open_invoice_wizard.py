0fe0cb06fa2034cb04ff1122947c8a4bc80c4afc
Francesco Apruzzese <cescoap@gmail.com>
2016-04-27 18:52:03 +0200

    [IMP] Fill open invoice report with informations keep from account move lines

--- account_financial_report_qweb/wizard/open_invoice_wizard.py
+++ account_financial_report_qweb/wizard/open_invoice_wizard.py
@@ -58,6 +58,66 @@ class OpenInvoiceWizard(models.TransientModel):
                 raise UserError(
                     'Until Date must be equal or greater than At Date')
 
+    @staticmethod
+    def _get_domain(data):
+        account_type = ('payable', 'receivable')
+        if data['result_selection'] == 'customer':
+            account_type = ('receivable', )
+        elif data['result_selection'] == 'supplier':
+            account_type = ('payable', )
+        domain = [
+            ('company_id', '=', data['company_id'].id),
+            ('move_id.date', '>=', data['at_date']),
+            ('move_id.date', '<=', data['until_date']),
+            ('account_id.user_type_id.type', 'in', account_type)
+            ]
+        if data['target_move'] != 'all':
+            domain.append(('move_id.state', 'in', ('posted', )), )
+        if data['partner_ids']:
+            domain.append(('partner_id', 'in', [p.id
+                                                for p
+                                                in data['partner_ids']]), )
+        return domain
+
+    @staticmethod
+    def _get_moves_data(move):
+        # return {
+        #     'date': data.date,
+        #     'period': data.invoice_id.period_id.name,
+        #     'journal': data.move_id.journal_id.name,
+        #     'reference': data.,
+        #     '': data.,
+        #     '': data.,
+        #     }
+        return {
+            'date': '',
+            'period': '',
+            'entry': '',
+            'journal': '',
+            'reference': '',
+            'label': '',
+            'rec': '',
+            'due_date': '',
+            'debit': '',
+            'credit': '',
+            'balance': '',
+            }
+
     @api.multi
     def print_report(self):
-        pass
+        self.ensure_one()
+        moves = self.env['account.move.line'].search(self._get_domain(self))
+        if not moves:
+            return True  # ----- Show a message here
+        datas = {}
+        for move in moves:
+            if move.account_id.name not in datas:
+                datas[move.account_id.name] = {}
+            if move.partner_id.name not in datas[move.account_id.name]:
+                datas[move.account_id.name][move.partner_id.name] = []
+            datas[move.account_id.name][move.partner_id.name].append(
+                self._get_moves_data(move))
+        return self.env['report'].get_action(
+            self, 'account_financial_report_qweb.open_invoice_report_qweb',
+            data={'data': datas})
+
