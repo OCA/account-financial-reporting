c45a174ebc040c503b07b909e245579b7636deb7
Stéphane Bidoul (ACSONE) <stephane.bidoul@acsone.eu>
2016-04-27 12:07:00 +0200

    mis_builder: add changelog (#174)

--- mis_builder/__openerp__.py
+++ mis_builder/__openerp__.py
@@ -4,7 +4,7 @@
 
 {
     'name': 'MIS Builder',
-    'version': '8.0.0.2.0',
+    'version': '8.0.1.0.0',
     'category': 'Reporting',
     'summary': """
         Build 'Management Information System' Reports and Dashboards
