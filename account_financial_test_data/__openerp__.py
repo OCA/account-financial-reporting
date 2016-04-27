59d832ab261e0a99998517d9e732c418d37a0f43
Kevin Broodkoorn <kb@taktik.com>
2016-04-27 15:51:36 +0200

    Initial commit

---
+++ account_financial_test_data/__openerp__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Account financial test data",
+    "summary": "Add all necessary data to test financial modules",
+    "version": "9.0.1.0.0",
+    "category": "Uncategorized",
+    "website": "https://odoo-community.org/",
+    "author": "Taktik, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "data": [
+    ]
+}
