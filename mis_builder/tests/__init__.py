6da30675e45f348917ebc164ee5f0caefdaf0900
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2016-04-30 15:20:03 +0200

    [IMP] mis_builder: add doctests to the test suite

--- mis_builder/tests/__init__.py
+++ mis_builder/tests/__init__.py
@@ -2,4 +2,7 @@
 # © 2014-2015 ACSONE SA/NV (<http://acsone.eu>)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
+from . import test_accounting_none
+from . import test_aggregate
 from . import test_mis_builder
+from . import test_simple_array
