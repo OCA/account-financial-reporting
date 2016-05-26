243fdc7685e9146b75e17fb500a11d8e64d26755
Leonardo Pistone <leonardo.pistone@camptocamp.com>
2016-05-26 11:11:03 +0200

    remove unused import

--- account_financial_report_qweb/wizard/open_invoice_wizard.py
+++ account_financial_report_qweb/wizard/open_invoice_wizard.py
@@ -5,7 +5,7 @@
 # Copyright 2016 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import models, fields, api, _
+from openerp import models, fields, api
 from openerp.exceptions import Warning as UserError
 from datetime import datetime
 
