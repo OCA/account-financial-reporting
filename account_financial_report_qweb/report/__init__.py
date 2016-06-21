af306bab1f7b33adda0b95650eaac1deed03e87c
jcoux <julien.coux@camptocamp.com>
2016-06-21 17:22:51 +0200

    Add OCA General/Partner Ledger PDF

--- account_financial_report_qweb/report/__init__.py
+++ account_financial_report_qweb/report/__init__.py
@@ -2,7 +2,6 @@
 # © 2015 Yannick Vaucher (Camptocamp)
 # © 2016 Damien Crier (Camptocamp)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).-
-from . import common
 from . import general_ledger
 from . import open_invoice
 from . import general_ledger_xlsx
--- account_financial_report_qweb/wizard/__init__.py
+++ account_financial_report_qweb/wizard/__init__.py
@@ -3,6 +3,6 @@
 # Copyright 2016 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from . import aged_partner_balance_wizard
-from . import ledger_report_wizard
 from . import balance_common_wizard
+from . import general_ledger_wizard
 from . import open_invoice_wizard
