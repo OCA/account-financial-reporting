4a01fb5bba2885f2dc754a39273d1914260acaf3
jcoux <julien.coux@camptocamp.com>
2016-08-01 09:58:49 +0200

    Refactoring and add new tests

--- account_financial_report_qweb/tests/__init__.py
+++ account_financial_report_qweb/tests/__init__.py
@@ -2,6 +2,7 @@
 # © 2016 Julien Coux (Camptocamp)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).-
 
+from . import abstract_test
 from . import test_aged_partner_balance
 from . import test_general_ledger
 from . import test_open_items
--- account_financial_report_qweb/tests/test_aged_partner_balance.py
+++ account_financial_report_qweb/tests/test_aged_partner_balance.py
@@ -4,71 +4,39 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 import time
-from openerp.tests.common import TransactionCase
+from . import abstract_test
 
 
-class TestAgedPartnerBalance(TransactionCase):
+class TestAgedPartnerBalance(abstract_test.AbstractTest):
+    """
+        Technical tests for Aged Partner Balance Report.
+    """
 
-    def setUp(cls):
-        super(TestAgedPartnerBalance, cls).setUp()
-        env = cls.env
-        model = env['report_aged_partner_balance_qweb']
-        main_company = env.ref('base.main_company')
+    def _getReportModel(self):
+        return self.env['report_aged_partner_balance_qweb']
 
-        cls.report = model.create({
+    def _getQwebReportName(self):
+        return 'account_financial_report_qweb.report_aged_partner_balance_qweb'
+
+    def _getXlsxReportName(self):
+        return 'account_financial_report_qweb.report_aged_partner_balance_xlsx'
+
+    def _getXlsxReportActionName(self):
+        return 'account_financial_report_qweb.' \
+               'action_report_aged_partner_balance_xlsx'
+
+    def _getReportTitle(self):
+        return 'Aged Partner Balance'
+
+    def _getBaseFilters(self):
+        return {
             'date_at': time.strftime('%Y-12-31'),
-            'company_id': main_company.id,
-        })
+            'company_id': self.env.ref('base.main_company').id,
+        }
 
-    def test_01_compute_data(self):
-        """Check if data are computed"""
-        self.report.compute_data_for_report()
-        self.assertGreaterEqual(len(self.report.account_ids), 1)
-
-    def test_02_generation_report_qweb(self):
-        """Check if report PDF/HTML is correctly generated"""
-
-        report_name = 'account_financial_report_qweb.' \
-                      'report_aged_partner_balance_qweb'
-        # Check if returned report action is correct
-        report_action = self.report.print_report()
-        self.assertDictContainsSubset(
-            {
-                'type': 'ir.actions.report.xml',
-                'report_name': report_name,
-                'report_type': 'qweb-pdf',
-            },
-            report_action
-        )
-
-        # Check if report template is correct
-        report_html = self.env['report'].get_html(self.report, report_name)
-        self.assertRegexpMatches(report_html, 'Aged Partner Balance')
-        self.assertRegexpMatches(report_html, self.report.account_ids[0].name)
-
-    def test_03_generation_report_xlsx(self):
-        """Check if report XLSX is correctly generated"""
-
-        report_name = 'account_financial_report_qweb.' \
-                      'report_aged_partner_balance_xlsx'
-        # Check if returned report action is correct
-        report_action = self.report.print_report(xlsx_report=True)
-        self.assertDictContainsSubset(
-            {
-                'type': 'ir.actions.report.xml',
-                'report_name': report_name,
-                'report_type': 'xlsx',
-            },
-            report_action
-        )
-
-        # Check if report template is correct
-        action_name = 'account_financial_report_qweb.' \
-                      'action_report_aged_partner_balance_xlsx'
-        report_xlsx = self.env.ref(action_name).render_report(
-            self.report.ids,
-            report_name,
-            {'report_type': u'xlsx'}
-        )
-        self.assertGreaterEqual(len(report_xlsx[0]), 1)
-        self.assertEqual(report_xlsx[1], 'xlsx')
+    def _getAdditionalFiltersToBeTested(self):
+        return [
+            {'only_posted_moves': True},
+            {'show_move_line_details': True},
+            {'only_posted_moves': True, 'show_move_line_details': True},
+        ]
