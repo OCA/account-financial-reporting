50b3b4577d56a48fae6f392ae8d501fe14a29e46
jcoux <julien.coux@camptocamp.com>
2016-08-01 09:58:49 +0200

    Refactoring and add new tests

--- account_financial_report_qweb/tests/test_general_ledger.py
+++ account_financial_report_qweb/tests/test_general_ledger.py
@@ -4,73 +4,49 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 import time
-from openerp.tests.common import TransactionCase
+from . import abstract_test
 
 
-class TestGeneralLedger(TransactionCase):
+class TestGeneralLedger(abstract_test.AbstractTest):
+    """
+        Technical tests for General Ledger Report.
+    """
 
-    def setUp(cls):
-        super(TestGeneralLedger, cls).setUp()
-        env = cls.env
-        model = env['report_general_ledger_qweb']
-        main_company = env.ref('base.main_company')
+    def _getReportModel(self):
+        return self.env['report_general_ledger_qweb']
 
-        cls.report = model.create({
+    def _getQwebReportName(self):
+        return 'account_financial_report_qweb.report_general_ledger_qweb'
+
+    def _getXlsxReportName(self):
+        return 'account_financial_report_qweb.report_general_ledger_xlsx'
+
+    def _getXlsxReportActionName(self):
+        return 'account_financial_report_qweb.' \
+               'action_report_general_ledger_xlsx'
+
+    def _getReportTitle(self):
+        return 'General Ledger'
+
+    def _getBaseFilters(self):
+        return {
             'date_from': time.strftime('%Y-01-01'),
             'date_to': time.strftime('%Y-12-31'),
-            'company_id': main_company.id,
+            'company_id': self.env.ref('base.main_company').id,
             'fy_start_date': time.strftime('%Y-01-01'),
-        })
+        }
 
-    def test_01_compute_data(self):
-        """Check if data are computed"""
-        self.report.compute_data_for_report()
-        self.assertGreaterEqual(len(self.report.account_ids), 1)
-
-    def test_02_generation_report_qweb(self):
-        """Check if report PDF/HTML is correctly generated"""
-
-        report_name = 'account_financial_report_qweb.' \
-                      'report_general_ledger_qweb'
-        # Check if returned report action is correct
-        report_action = self.report.print_report(xlsx_report=False)
-        self.assertDictContainsSubset(
+    def _getAdditionalFiltersToBeTested(self):
+        return [
+            {'only_posted_moves': True},
+            {'hide_account_balance_at_0': True},
+            {'centralize': True},
+            {'only_posted_moves': True, 'hide_account_balance_at_0': True},
+            {'only_posted_moves': True, 'centralize': True},
+            {'hide_account_balance_at_0': True, 'centralize': True},
             {
-                'type': 'ir.actions.report.xml',
-                'report_name': report_name,
-                'report_type': 'qweb-pdf',
+                'only_posted_moves': True,
+                'hide_account_balance_at_0': True,
+                'centralize': True
             },
-            report_action
-        )
-
-        # Check if report template is correct
-        report_html = self.env['report'].get_html(self.report, report_name)
-        self.assertRegexpMatches(report_html, 'General Ledger')
-        self.assertRegexpMatches(report_html, self.report.account_ids[0].name)
-
-    def test_03_generation_report_xlsx(self):
-        """Check if report XLSX is correctly generated"""
-
-        report_name = 'account_financial_report_qweb.' \
-                      'report_general_ledger_xlsx'
-        # Check if returned report action is correct
-        report_action = self.report.print_report(xlsx_report=True)
-        self.assertDictContainsSubset(
-            {
-                'type': 'ir.actions.report.xml',
-                'report_name': report_name,
-                'report_type': 'xlsx',
-            },
-            report_action
-        )
-
-        # Check if report template is correct
-        action_name = 'account_financial_report_qweb.' \
-                      'action_report_general_ledger_xlsx'
-        report_xlsx = self.env.ref(action_name).render_report(
-            self.report.ids,
-            report_name,
-            {'report_type': u'xlsx'}
-        )
-        self.assertGreaterEqual(len(report_xlsx[0]), 1)
-        self.assertEqual(report_xlsx[1], 'xlsx')
+        ]
--- account_financial_report_qweb/tests/test_open_items.py
+++ account_financial_report_qweb/tests/test_open_items.py
@@ -4,71 +4,38 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 import time
-from openerp.tests.common import TransactionCase
+from . import abstract_test
 
 
-class TestOpenItems(TransactionCase):
+class TestOpenItems(abstract_test.AbstractTest):
+    """
+        Technical tests for Open Items Report.
+    """
 
-    def setUp(cls):
-        super(TestOpenItems, cls).setUp()
-        env = cls.env
-        model = env['report_open_items_qweb']
-        main_company = env.ref('base.main_company')
+    def _getReportModel(self):
+        return self.env['report_open_items_qweb']
 
-        cls.report = model.create({
+    def _getQwebReportName(self):
+        return 'account_financial_report_qweb.report_open_items_qweb'
+
+    def _getXlsxReportName(self):
+        return 'account_financial_report_qweb.report_open_items_xlsx'
+
+    def _getXlsxReportActionName(self):
+        return 'account_financial_report_qweb.action_report_open_items_xlsx'
+
+    def _getReportTitle(self):
+        return 'Open Items'
+
+    def _getBaseFilters(self):
+        return {
             'date_at': time.strftime('%Y-12-31'),
-            'company_id': main_company.id,
-        })
+            'company_id': self.env.ref('base.main_company').id,
+        }
 
-    def test_01_compute_data(self):
-        """Check if data are computed"""
-        self.report.compute_data_for_report()
-        self.assertGreaterEqual(len(self.report.account_ids), 1)
-
-    def test_02_generation_report_qweb(self):
-        """Check if report PDF/HTML is correctly generated"""
-
-        report_name = 'account_financial_report_qweb.' \
-                      'report_open_items_qweb'
-        # Check if returned report action is correct
-        report_action = self.report.print_report()
-        self.assertDictContainsSubset(
-            {
-                'type': 'ir.actions.report.xml',
-                'report_name': report_name,
-                'report_type': 'qweb-pdf',
-            },
-            report_action
-        )
-
-        # Check if report template is correct
-        report_html = self.env['report'].get_html(self.report, report_name)
-        self.assertRegexpMatches(report_html, 'Open Items')
-        self.assertRegexpMatches(report_html, self.report.account_ids[0].name)
-
-    def test_03_generation_report_xlsx(self):
-        """Check if report XLSX is correctly generated"""
-
-        report_name = 'account_financial_report_qweb.' \
-                      'report_open_items_xlsx'
-        # Check if returned report action is correct
-        report_action = self.report.print_report(xlsx_report=True)
-        self.assertDictContainsSubset(
-            {
-                'type': 'ir.actions.report.xml',
-                'report_name': report_name,
-                'report_type': 'xlsx',
-            },
-            report_action
-        )
-
-        # Check if report template is correct
-        action_name = 'account_financial_report_qweb.' \
-                      'action_report_open_items_xlsx'
-        report_xlsx = self.env.ref(action_name).render_report(
-            self.report.ids,
-            report_name,
-            {'report_type': u'xlsx'}
-        )
-        self.assertGreaterEqual(len(report_xlsx[0]), 1)
-        self.assertEqual(report_xlsx[1], 'xlsx')
+    def _getAdditionalFiltersToBeTested(self):
+        return [
+            {'only_posted_moves': True},
+            {'hide_account_balance_at_0': True},
+            {'only_posted_moves': True, 'hide_account_balance_at_0': True},
+        ]
