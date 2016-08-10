2af9293e78a5351e87197a036827f7a2c9e8133b
jcoux <julien.coux@camptocamp.com>
2016-08-10 10:35:47 +0200

    Fix module version and delete useless section on readme

--- account_financial_report_qweb/__openerp__.py
+++ account_financial_report_qweb/__openerp__.py
@@ -5,7 +5,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': 'QWeb Financial Reports',
-    'version': '9.0.0.1.0',
+    'version': '9.0.1.0.0',
     'category': 'Reporting',
     'summary': 'OCA Financial Reports',
     'author': 'Camptocamp SA,'
