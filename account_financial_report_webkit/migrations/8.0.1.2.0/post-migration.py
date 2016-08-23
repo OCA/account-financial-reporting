f230cc83b93ff0ffaed46005bd219ea8c38024f7
dufresnedavid <david.dufresne@savoirfairelinux.com>
2016-08-23 10:42:02 -0400

    Improve migration script 8.0.1.2.0

--- account_financial_report_webkit/migrations/8.0.1.2.0/post-migration.py
+++ account_financial_report_webkit/migrations/8.0.1.2.0/post-migration.py
@@ -2,12 +2,41 @@
 # © 2016 Savoir-faire Linux
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import api, SUPERUSER_ID
+import logging
+
+_logger = logging.getLogger(__name__)
 
 
 def migrate(cr, version):
     if not version:
         return
 
-    env = api.Environment(cr, SUPERUSER_ID, {})
-    env['account.move.line'].search([])._compute_last_rec_date()
+    _logger.info('Updating column last_rec_date on account_move_line')
+
+    cr.execute(
+        """
+        UPDATE account_move_line SET last_rec_date = rec_data.aml_date
+        FROM (
+            SELECT rec.id, max(aml.date) as aml_date
+            FROM account_move_line aml
+            JOIN account_move_reconcile rec
+            ON rec.id = aml.reconcile_id
+            GROUP BY rec.id
+        ) as rec_data
+        WHERE rec_data.id = account_move_line.reconcile_id
+        """
+    )
+
+    cr.execute(
+        """
+        UPDATE account_move_line SET last_rec_date = rec_data.aml_date
+        FROM (
+            SELECT rec.id, max(aml.date) as aml_date
+            FROM account_move_line aml
+            JOIN account_move_reconcile rec
+            ON rec.id = aml.reconcile_partial_id
+            GROUP BY rec.id
+        ) as rec_data
+        WHERE rec_data.id = account_move_line.reconcile_partial_id
+        """
+    )
