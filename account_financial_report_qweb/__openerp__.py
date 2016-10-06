353b904cd1b72e3c3afbb13fc406d9ca014f18e7
Pedro M. Baeza <pedro.baeza@gmail.com>
2016-10-06 14:45:29 +0200

    [MIG] Make modules uninstallable

--- account_financial_report_qweb/__openerp__.py
+++ account_financial_report_qweb/__openerp__.py
@@ -37,7 +37,7 @@
     ],
     'test': [
     ],
-    'installable': True,
+    'installable': False,
     'application': True,
     'auto_install': False,
     'license': 'AGPL-3',
--- mis_builder/__openerp__.py
+++ mis_builder/__openerp__.py
@@ -40,7 +40,7 @@
     'qweb': [
         'static/src/xml/*.xml'
     ],
-    'installable': True,
+    'installable': False,
     'application': True,
     'license': 'AGPL-3',
 }
