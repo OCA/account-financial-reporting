572a54c23af9894604c7dba8f9aa0d3dac980701
Adrien Peiffer (ACSONE) <adrien.peiffer@acsone.eu>
2016-10-11 10:06:24 +0200

    [CHG] Flake8

--- mis_builder_analytic_filter/models/mis_report_instance.py
+++ mis_builder_analytic_filter/models/mis_report_instance.py
@@ -2,7 +2,7 @@
 # Copyright 2016 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import api, fields, models, _
+from openerp import api, fields, models
 
 
 class MisReportInstance(models.Model):
