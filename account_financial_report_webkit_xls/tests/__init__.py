470af456589e08c4e55aeb07f90fc20fa6494048
luc-demeyer <luc.demeyer@noviat.com>
2017-01-01 20:46:36 +0100

    increase coveralls

--- account_financial_report_webkit_xls/tests/__init__.py
+++ account_financial_report_webkit_xls/tests/__init__.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
 from . import test_general_leger_xls
 from . import test_partner_ledger_xls
+from . import test_trial_balance_xls
+from . import test_partner_balance_xls
+from . import test_open_invoices_xls
 from . import test_aged_partner_balance_xls
