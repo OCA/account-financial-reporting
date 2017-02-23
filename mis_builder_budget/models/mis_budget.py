0aa44fe7f60bbd6e7dc487544ac95b0a4d3e3bf8
Stéphane Bidoul (ACSONE) <stephane.bidoul@acsone.eu>
2017-02-23 18:40:44 +0100

    [ADD] mis_builder_budget: basic workflow

--- mis_builder_budget/models/mis_budget.py
+++ mis_builder_budget/models/mis_budget.py
@@ -2,20 +2,21 @@
 # Copyright 2017 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models
+from odoo import api, fields, models, _
 
 
 class MisBudget(models.Model):
 
     _name = 'mis.budget'
     _description = 'MIS Budget'
-
-    # TODO chatter, track state, name, description
+    _inherit = ['mail.thread']
 
     name = fields.Char(
         required=True,
+        track_visibility='onchange',
     )
     description = fields.Char(
+        track_visibility='onchange',
     )
     report_id = fields.Many2one(
         comodel_name='mis.report',
@@ -29,23 +30,35 @@ class MisBudget(models.Model):
     date_from = fields.Date(
         required=True,
         string='From',
+        track_visibility='onchange',
     )
     date_to = fields.Date(
         required=True,
         string='To',
+        track_visibility='onchange',
     )
     state = fields.Selection(
         [('draft', 'Draft'),
          ('confirmed', 'Confirmed'),
-         ('validated', 'Validated')],
+         ('cancelled', 'Cancelled')],
         required=True,
-        default='draft'
+        default='draft',
+        track_visibility='onchange',
     )
     item_ids = fields.One2many(
         comodel_name='mis.budget.item',
         inverse_name='budget_id',
-        copy=True
+        copy=True,
     )
+
+    @api.multi
+    def copy(self, default=None):
+        self.ensure_one()
+        if default is None:
+            default = {}
+        if 'name' not in default:
+            default['name'] = _("%s (copy)") % self.name
+        return super(MisBudget, self).copy(default=default)
 
     @api.onchange('date_range_id')
     def _onchange_date_range(self):
@@ -61,3 +74,18 @@ class MisBudget(models.Model):
                 if rec.date_from != rec.date_range_id.date_start or \
                         rec.date_to != rec.date_range_id.date_end:
                     rec.date_range_id = False
+
+    @api.multi
+    def action_draft(self):
+        for rec in self:
+            self.state = 'draft'
+
+    @api.multi
+    def action_cancel(self):
+        for rec in self:
+            self.state = 'cancelled'
+
+    @api.multi
+    def action_confirm(self):
+        for rec in self:
+            self.state = 'confirmed'
