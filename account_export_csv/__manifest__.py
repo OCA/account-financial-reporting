e543d08aaaaa7cf08fc04c448626a6bf48aa406e
Adrien Peiffer (ACSONE) <adrien.peiffer@acsone.eu>
2017-05-24 11:11:50 +0200

    [MIG] Migration of account_export_csv.

--- account_export_csv/__manifest__.py
+++ account_export_csv/__manifest__.py
@@ -1,51 +1,20 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Author Joel Grand-Guillaume and Vincent Renaville
-#    Copyright 2013 Camptocamp SA
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# Copyright 2013 Camptocamp SA
+# Copyright 2017 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Account Export CSV',
-    'version': '8.0.1.1.0',
+    'summary': "Adds accounting CSV export",
+    'version': '10.0.1.1.0',
     'depends': [
         'account',
     ],
     'author': "Camptocamp,Odoo Community Association (OCA)",
-    'description': """
-
-    Add a wizard that allow you to export a csv file based on accounting
-    journal entries
-
-    - Trial Balance
-    - Analytic Balance (with accounts)
-    - Journal Entries
-
-    You can filter by period
-
-    TODO: rearange wizard view with only one button to generate file plus
-    define a selection list to select report type
-    """,
     'website': 'http://www.camptocamp.com',
     'license': 'AGPL-3',
     'data': [
         'wizard/account_export_csv_view.xml',
-        'menu.xml',
     ],
-    'installable': False,
-    'active': False,
+    'installable': True,
 }
