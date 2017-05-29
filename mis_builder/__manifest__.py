8fd7e1f75e163b3d171cdcd257aec8506114aabd
Stéphane Bidoul (ACSONE) <stephane.bidoul@acsone.eu>
2017-05-29 17:23:29 +0200

    bump version

--- mis_builder/__manifest__.py
+++ mis_builder/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     'name': 'MIS Builder',
-    'version': '10.0.2.0.2',
+    'version': '10.0.3.0.0',
     'category': 'Reporting',
     'summary': """
         Build 'Management Information System' Reports and Dashboards
