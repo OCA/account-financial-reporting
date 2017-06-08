17f18a3114329f1f5b09085da714c8d8264821d9
Alexis de Lattre <alexis.delattre@akretion.com>
2017-06-09 00:05:30 +0200

    Add module account_bank_reconciliation_summary_xlsx

---
+++ account_bank_reconciliation_summary_xlsx/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# © 2017 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Account Bank Statement Reconciliation Summary',
+    'version': '10.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': "Akretion,Odoo Community Association (OCA)",
+    'website': 'http://www.akretion.com',
+    'summary': 'Adds an XLSX report to help on bank statement reconciliation',
+    'depends': ['account', 'report_xlsx'],
+    'data': [
+        'report/report.xml',
+        'views/account_bank_statement_view.xml',
+        ],
+    'installable': True,
+}
---
+++ account_bank_reconciliation_summary_xlsx/report/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+
+from . import bank_reconciliation_xlsx
