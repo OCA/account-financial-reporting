28e295f6caf3061d53396561878345aaccc2c55e
mreficent <miquel.raich@eficent.com>
2017-06-16 12:32:20 +0200

    [MIG] customer_activity_statement: Migration to 10.0

--- customer_activity_statement/__manifest__.py
+++ customer_activity_statement/__manifest__.py
@@ -5,7 +5,7 @@
 
 {
     'name': 'Customer Activity Statement',
-    'version': '9.0.1.0.0',
+    'version': '10.0.1.0.0',
     'category': 'Accounting & Finance',
     'summary': 'OCA Financial Reports',
     'author': "Eficent, Odoo Community Association (OCA)",
