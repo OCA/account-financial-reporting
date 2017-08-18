68302178ae20d3a7401962fd9b537c2e30d1ac24
mreficent <miquel.raich@eficent.com>
2017-08-18 13:48:46 +0200

    [10.0][MIG] account_financial_report_horizontal

--- account_financial_report_horizontal/models/account_financial_report.py
+++ account_financial_report_horizontal/models/account_financial_report.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 # © 2015 Therp BV <http://therp.nl>
-from openerp import models, api
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from odoo import api, models
 
 
 class AccountFinancialReport(models.Model):
