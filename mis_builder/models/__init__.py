a4568427c8cfd5dbae518e72e94b08675bdfb9d8
Stéphane Bidoul (ACSONE) <stephane.bidoul@acsone.eu>
2017-09-30 16:40:55 +0200

    [DEL] mis_builder moved to OCA/mis-builder

--- mis_builder/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# © 2014-2015 ACSONE SA/NV (<http://acsone.eu>)
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-
-from . import mis_report
-from . import mis_report_instance
-from . import mis_report_style
-from . import aep
