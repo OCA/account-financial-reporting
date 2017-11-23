4e1d5bd6211e00dbbe3bdb2c917912b35da0c2c7
Jordi Ballester <jordi.ballester@eficent.com>
2017-11-23 16:23:46 +0100

    migrate account_financial_report_qweb to 11.0

---
+++ account_financial_report/wizard/general_ledger_wizard.py
@@ -0,0 +1,147 @@
+
+# Author: Damien Crier
+# Author: Julien Coux
+# Copyright 2016 Camptocamp SA
+# Copyright 2017 Akretion - Alexis de Lattre
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import models, fields, api
+
+
+class GeneralLedgerReportWizard(models.TransientModel):
+    """General ledger report wizard."""
+
+    _name = "general.ledger.report.wizard"
+    _description = "General Ledger Report Wizard"
+
+    company_id = fields.Many2one(
+        comodel_name='res.company',
+        default=lambda self: self.env.user.company_id,
+        string='Company'
+    )
+    date_range_id = fields.Many2one(
+        comodel_name='date.range',
+        string='Date range'
+    )
+    date_from = fields.Date(required=True)
+    date_to = fields.Date(required=True)
+    fy_start_date = fields.Date(compute='_compute_fy_start_date')
+    target_move = fields.Selection([('posted', 'All Posted Entries'),
+                                    ('all', 'All Entries')],
+                                   string='Target Moves',
+                                   required=True,
+                                   default='all')
+    account_ids = fields.Many2many(
+        comodel_name='account.account',
+        string='Filter accounts',
+    )
+    centralize = fields.Boolean(string='Activate centralization',
+                                default=True)
+    hide_account_balance_at_0 = fields.Boolean(
+        string='Hide account ending balance at 0',
+        help='Use this filter to hide an account or a partner '
+             'with an ending balance at 0. '
+             'If partners are filtered, '
+             'debits and credits totals will not match the trial balance.'
+    )
+    receivable_accounts_only = fields.Boolean()
+    payable_accounts_only = fields.Boolean()
+    partner_ids = fields.Many2many(
+        comodel_name='res.partner',
+        string='Filter partners',
+    )
+    cost_center_ids = fields.Many2many(
+        comodel_name='account.analytic.account',
+        string='Filter cost centers',
+    )
+
+    not_only_one_unaffected_earnings_account = fields.Boolean(
+        readonly=True,
+        string='Not only one unaffected earnings account'
+    )
+
+    @api.depends('date_from')
+    def _compute_fy_start_date(self):
+        for wiz in self.filtered('date_from'):
+            date = fields.Datetime.from_string(wiz.date_from)
+            res = self.company_id.compute_fiscalyear_dates(date)
+            wiz.fy_start_date = res['date_from']
+
+    @api.onchange('company_id')
+    def onchange_company_id(self):
+        """Handle company change."""
+        account_type = self.env.ref('account.data_unaffected_earnings')
+        count = self.env['account.account'].search_count(
+            [
+                ('user_type_id', '=', account_type.id),
+                ('company_id', '=', self.company_id.id)
+            ])
+        self.not_only_one_unaffected_earnings_account = count != 1
+
+    @api.onchange('date_range_id')
+    def onchange_date_range_id(self):
+        """Handle date range change."""
+        self.date_from = self.date_range_id.date_start
+        self.date_to = self.date_range_id.date_end
+
+    @api.onchange('receivable_accounts_only', 'payable_accounts_only')
+    def onchange_type_accounts_only(self):
+        """Handle receivable/payable accounts only change."""
+        if self.receivable_accounts_only or self.payable_accounts_only:
+            domain = []
+            if self.receivable_accounts_only and self.payable_accounts_only:
+                domain += [('internal_type', 'in', ('receivable', 'payable'))]
+            elif self.receivable_accounts_only:
+                domain += [('internal_type', '=', 'receivable')]
+            elif self.payable_accounts_only:
+                domain += [('internal_type', '=', 'payable')]
+            self.account_ids = self.env['account.account'].search(domain)
+        else:
+            self.account_ids = None
+
+    @api.onchange('partner_ids')
+    def onchange_partner_ids(self):
+        """Handle partners change."""
+        if self.partner_ids:
+            self.receivable_accounts_only = self.payable_accounts_only = True
+        else:
+            self.receivable_accounts_only = self.payable_accounts_only = False
+
+    @api.multi
+    def button_export_html(self):
+        self.ensure_one()
+        report_type = 'qweb-html'
+        return self._export(report_type)
+
+    @api.multi
+    def button_export_pdf(self):
+        self.ensure_one()
+        report_type = 'qweb-pdf'
+        return self._export(report_type)
+
+    @api.multi
+    def button_export_xlsx(self):
+        self.ensure_one()
+        report_type = 'xlsx'
+        return self._export(report_type)
+
+    def _prepare_report_general_ledger(self):
+        self.ensure_one()
+        return {
+            'date_from': self.date_from,
+            'date_to': self.date_to,
+            'only_posted_moves': self.target_move == 'posted',
+            'hide_account_balance_at_0': self.hide_account_balance_at_0,
+            'company_id': self.company_id.id,
+            'filter_account_ids': [(6, 0, self.account_ids.ids)],
+            'filter_partner_ids': [(6, 0, self.partner_ids.ids)],
+            'filter_cost_center_ids': [(6, 0, self.cost_center_ids.ids)],
+            'centralize': self.centralize,
+            'fy_start_date': self.fy_start_date,
+        }
+
+    def _export(self, report_type):
+        """Default export is PDF."""
+        model = self.env['report_general_ledger']
+        report = model.create(self._prepare_report_general_ledger())
+        return report.print_report(report_type)
--- account_financial_report_qweb/wizard/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# Author: Damien Crier
-# Author: Julien Coux
-# Copyright 2016 Camptocamp SA
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-from . import aged_partner_balance_wizard
-from . import general_ledger_wizard
-from . import open_items_wizard
-from . import trial_balance_wizard
