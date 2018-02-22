dd0d407ceba24ac9d64368985b7be70584611054
Adrien Peiffer (ACSONE) <adrien.peiffer@acsone.eu>
2018-02-22 14:10:19 +0100

    [10.0] AFR Aged partner balance : columns and datas consistency. (#383)

--- account_financial_report/__manifest__.py
+++ account_financial_report/__manifest__.py
@@ -5,7 +5,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': 'Account Financial Reports',
-    'version': '11.0.1.1.0',
+    'version': '11.0.1.2.0',
     'category': 'Reporting',
     'summary': 'OCA Financial Reports',
     'author': 'Camptocamp SA,'
