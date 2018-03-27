68e6c5c8bb5df14c615f4d3b53a6bbbd125fb946
hveficent <hector.villarreal@eficent.com>
2018-03-27 12:19:57 +0200

    utf-8 remove from .py files

--- account_tax_balance/models/account_tax.py
+++ account_tax_balance/models/account_tax.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # © 2016 Lorenzo Battistini - Agile Business Group
 # © 2016 Antonio Espinosa <antonio.espinosa@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
