ade18fac8949c45f6c0da7f84f9ce2d86a708f9e
hveficent <hector.villarreal@eficent.com>
2018-05-30 16:54:09 +0200

    [11.0][IMP] account_financial_report - foreign_currency

--- account_financial_report/wizard/__init__.py
+++ account_financial_report/wizard/__init__.py
@@ -1,5 +1,6 @@
 from . import aged_partner_balance_wizard
 from . import general_ledger_wizard
+from . import journal_ledger_wizard
 from . import open_items_wizard
 from . import trial_balance_wizard
 from . import vat_report_wizard
