1565647b65c39aaa818e05956f25842dac525522
oleksandrpaziuk <o.paziuk@mobilunity.com>
2018-06-04 13:51:01 +0300

    fixup! fixup! fixup! [10.0] add account_bank_statement_line_reconciliation module

--- account_bank_statement_line_reconciliation/wizard/account_bank_statement_line_reconciliation_wizard.py
+++ account_bank_statement_line_reconciliation/wizard/account_bank_statement_line_reconciliation_wizard.py
@@ -11,28 +11,31 @@ class AccountBankStatementLineReconciliationWizard(models.TransientModel):
 
     def _account_move_ids_and_journal_id(self):
         ids = self._context.get('active_ids')
+        account_move_ids = self.env['account.move']
+        journal_id = self.env['account.journal']
         if ids:
-            account_move_ids = self.env['account.move'].browse(ids)
+            account_move_ids = account_move_ids.browse(ids)
 
-            journal_ids = list(set(account_move_ids.mapped('journal_id.id')))
+            journal_ids = account_move_ids.mapped('journal_id')
 
             if len(account_move_ids) > 1 and len(journal_ids) > 1:
                 msg = _("Please only select Journal entries "
                         "that belongs to the same bank journal")
                 raise ValidationError(msg)
 
-            journal_id = self.env['account.journal'].browse(journal_ids[0])
+            journal_id = journal_ids[0]
 
-            return account_move_ids, journal_id
-        return [], False
+        return account_move_ids, journal_id
+
+    def _default_account_move_ids(self):
+        account_move_ids, __ = self._account_move_ids_and_journal_id()
+        return account_move_ids
 
     def _default_statement_line_ids(self):
-        account_move_ids, _tmp = self._account_move_ids_and_journal_id()
-
-        return account_move_ids.mapped('statement_line_id')
+        return self._default_account_move_ids.mapped('statement_line_id')
 
     def _domain_new_statement_line_id(self):
-        _tmp, journal_id = self._account_move_ids_and_journal_id()
+        __, journal_id = self._account_move_ids_and_journal_id()
 
         if journal_id:
             self.env.cr.execute("""
@@ -45,11 +48,16 @@ class AccountBankStatementLineReconciliationWizard(models.TransientModel):
             return "[('id','in',{ids})]".format(ids=statement_line_ids)
         return "[]"
 
+    account_move_ids = fields.One2many(
+        'account.move',
+        default=_default_account_move_ids,
+        readonly=True
+    )
+
     statement_line_ids = fields.One2many(
         'account.bank.statement.line',
         string=_('Current values'),
-        default=_default_statement_line_ids,
-        compute='_default_statement_line_ids'
+        default=_default_statement_line_ids
     )
 
     new_statement_line_id = fields.Many2one(
@@ -60,7 +68,7 @@ class AccountBankStatementLineReconciliationWizard(models.TransientModel):
 
     @api.multi
     def set_new_statement_line_value(self):
-        account_move_ids, _tmp = self._account_move_ids_and_journal_id()
-        for account_move_id in account_move_ids:
-            account_move_id.statement_line_id = self.new_statement_line_id
+        self.account_move_ids.write({
+            'statement_line_id': self.new_statement_line_id.id,
+        })
         return {}
