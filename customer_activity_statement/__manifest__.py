bd469eb507850ac14eecdfea57b1a50fd0fd7608
Jordi Ballester Alomar <jordi.ballester@eficent.com>
2018-06-22 01:49:24 +0200

    [IMP] Extend customer_activity_statement to display also payables

--- customer_activity_statement/__manifest__.py
+++ customer_activity_statement/__manifest__.py
@@ -3,8 +3,8 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'Customer Activity Statement',
-    'version': '11.0.1.0.0',
+    'name': 'Partner Activity Statement',
+    'version': '11.0.2.0.0',
     'category': 'Accounting & Finance',
     'summary': 'OCA Financial Reports',
     'author': "Eficent, Odoo Community Association (OCA)",
