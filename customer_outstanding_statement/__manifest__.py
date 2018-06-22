c6b6f6b566e02157d12d384dae9f2d1c762759b8
mreficent <miquel.raich@eficent.com>
2018-06-22 17:23:15 +0200

    [IMP] Extend customer_outstanding_statement to display also payables

--- customer_outstanding_statement/__manifest__.py
+++ customer_outstanding_statement/__manifest__.py
@@ -3,8 +3,8 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'Customer Outstanding Statement',
-    'version': '11.0.1.0.0',
+    'name': 'Partner Outstanding Statement',
+    'version': '11.0.2.0.0',
     'category': 'Accounting & Finance',
     'summary': 'OCA Financial Reports',
     'author': "Eficent, Odoo Community Association (OCA)",
