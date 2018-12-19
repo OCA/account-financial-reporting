6606fd935a4ba896817cc7f0baca1d2ca95c8757
Jordi Ballester Alomar <jordi.ballester@eficent.com>
2018-12-19 10:53:09 +0100

    add proper titles

--- account_financial_report/report/general_ledger_xlsx.py
+++ account_financial_report/report/general_ledger_xlsx.py
@@ -11,8 +11,10 @@ class GeneralLedgerXslx(models.AbstractModel):
     _name = 'report.a_f_r.report_general_ledger_xlsx'
     _inherit = 'report.account_financial_report.abstract_report_xlsx'
 
-    def _get_report_name(self):
-        return _('General Ledger')
+    def _get_report_name(self, objects):
+        report = objects
+        return _('General Ledger - %s - %s') % (
+            report.company_id.name, report.company_id.currency_id.name)
 
     def _get_report_columns(self, report):
         res = {
