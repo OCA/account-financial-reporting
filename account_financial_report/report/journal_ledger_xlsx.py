aad9bfa16514ff8f8c225937f4288910ea62734e
Jordi Ballester Alomar <jordi.ballester@eficent.com>
2018-12-19 10:53:09 +0100

    add proper titles

--- account_financial_report/report/journal_ledger_xlsx.py
+++ account_financial_report/report/journal_ledger_xlsx.py
@@ -10,8 +10,10 @@ class JournalLedgerXslx(models.AbstractModel):
     _name = 'report.a_f_r.report_journal_ledger_xlsx'
     _inherit = 'report.account_financial_report.abstract_report_xlsx'
 
-    def _get_report_name(self):
-        return _('Journal Ledger')
+    def _get_report_name(self, objects):
+        report = objects
+        return _('Journal Ledger - %s - %s') % (
+            report.company_id.name, report.company_id.currency_id.name)
 
     def _get_report_columns(self, report):
         columns = [
