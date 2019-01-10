18baf50ee52c60a347179e43e1de4f9cb30bc231
Jordi Ballester Alomar <jordi.ballester@eficent.com>
2019-01-10 19:11:24 +0100

    [account_financial_report] fix title formatting for all reports

--- account_financial_report/report/trial_balance_xlsx.py
+++ account_financial_report/report/trial_balance_xlsx.py
@@ -11,8 +11,8 @@ class TrialBalanceXslx(models.AbstractModel):
     _inherit = 'report.account_financial_report.abstract_report_xlsx'
 
     def _get_report_name(self, report):
-        return _('Trial Balance - %s - %s') % (
-            report.company_id.name, report.company_id.currency_id.name)
+        report_name = _('Trial Balance')
+        return self._get_report_complete_name(report, report_name)
 
     def _get_report_columns(self, report):
         if not report.show_partner_details:
