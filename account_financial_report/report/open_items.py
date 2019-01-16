691edfa2bc362c8b3d7e881d823d9808dcf77fbd
Wolfgang Pichler <wpichler@callino.at>
2019-01-16 19:38:26 +0100

    [MIG] account_financial_report: Migration to 12.0
    [IMP] Reformated one query to avoid one sql injection warning message - but no sql injection was possible here

--- account_financial_report/report/open_items.py
+++ account_financial_report/report/open_items.py
@@ -160,7 +160,7 @@ class OpenItemsReportCompute(models.TransientModel):
                           'report_open_items_qweb'
         return self.env['ir.actions.report'].search(
             [('report_name', '=', report_name),
-             ('report_type', '=', report_type)], limit=1).report_action(self)
+             ('report_type', '=', report_type)], limit=1).report_action(self, config=False)
 
     def _get_html(self):
         result = {}
