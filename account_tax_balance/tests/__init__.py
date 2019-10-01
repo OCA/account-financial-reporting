e73ac973ff8b0fb7d6d2b43a7f0db6ea032b1073
Andrea <a.stirpe@onestein.nl>
2019-10-01 09:28:22 +0200

    Code review

--- account_tax_balance/tests/__init__.py
+++ account_tax_balance/tests/__init__.py
@@ -1,5 +1,3 @@
-# © 2016 Lorenzo Battistini - Agile Business Group
-# © 2016 Giovanni Capalbo
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from . import test_account_tax_balance
