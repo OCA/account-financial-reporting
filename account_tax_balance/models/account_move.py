321c06c5c371dc18e8e3b8a1c0eb1d0cddcc9c8e
Andrea <a.stirpe@onestein.nl>
2019-10-01 09:28:22 +0200

    Code review

--- account_tax_balance/models/account_move.py
+++ account_tax_balance/models/account_move.py
@@ -1,4 +1,4 @@
-# © 2016 Antonio Espinosa <antonio.espinosa@tecnativa.com>
+# Copyright 2016 Antonio Espinosa <antonio.espinosa@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import api, fields, models
--- account_tax_balance/wizard/open_tax_balances.py
+++ account_tax_balance/wizard/open_tax_balances.py
@@ -1,4 +1,4 @@
-# © 2016 Lorenzo Battistini - Agile Business Group
+# Copyright 2016 Lorenzo Battistini - Agile Business Group
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import _, api, fields, models
