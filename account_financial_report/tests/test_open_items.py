823d7b5d888b28040d020fcd8af9928e95ff81df
Joan Sisquella <joan.sisquella@forgeflow.com>
2020-01-23 17:27:52 +0100

    [12.0] account_financial_report: refactor

--- account_financial_report/tests/test_open_items.py
+++ account_financial_report/tests/test_open_items.py
@@ -2,43 +2,10 @@
 # Copyright 2016 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from datetime import date
-from . import abstract_test_foreign_currency as a_t_f_c
+from odoo.tests.common import TransactionCase
 
 
-class TestOpenItems(a_t_f_c.AbstractTestForeignCurrency):
-    """
-        Technical tests for Open Items Report.
-    """
-
-    def _getReportModel(self):
-        return self.env['report_open_items']
-
-    def _getQwebReportName(self):
-        return 'account_financial_report.report_open_items_qweb'
-
-    def _getXlsxReportName(self):
-        return 'a_f_r.report_open_items_xlsx'
-
-    def _getXlsxReportActionName(self):
-        return 'account_financial_report.action_report_open_items_xlsx'
-
-    def _getReportTitle(self):
-        return 'Odoo'
-
-    def _getBaseFilters(self):
-        return {
-            'date_at': date(date.today().year, 12, 31),
-            'company_id': self.company.id,
-            'foreign_currency': True,
-        }
-
-    def _getAdditionalFiltersToBeTested(self):
-        return [
-            {'only_posted_moves': True},
-            {'hide_account_at_0': True},
-            {'only_posted_moves': True, 'hide_account_at_0': True},
-        ]
+class TestOpenItems(TransactionCase):
 
     def test_partner_filter(self):
         partner_1 = self.env.ref('base.res_partner_1')
