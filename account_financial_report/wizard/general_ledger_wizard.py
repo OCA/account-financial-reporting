f21b54345078ed7605f98120622129ef42db61e5
Ernesto Tejeda <ernesto.tejeda@tecnativa.com>
2020-03-23 11:14:52 -0400

    [IMP] account_financial_report: black, isort

--- account_financial_report/wizard/general_ledger_wizard.py
+++ account_financial_report/wizard/general_ledger_wizard.py
@@ -7,9 +7,10 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 
-from odoo import api, fields, models, _
-from odoo.exceptions import ValidationError
 import time
+
+from odoo import _, api, fields, models
+from odoo.exceptions import ValidationError
 
 
 class GeneralLedgerReportWizard(models.TransientModel):
@@ -17,73 +18,61 @@ class GeneralLedgerReportWizard(models.TransientModel):
 
     _name = "general.ledger.report.wizard"
     _description = "General Ledger Report Wizard"
-    _inherit = 'account_financial_report_abstract_wizard'
+    _inherit = "account_financial_report_abstract_wizard"
 
     company_id = fields.Many2one(
-        comodel_name='res.company',
+        comodel_name="res.company",
         default=lambda self: self.env.user.company_id,
         required=False,
-        string='Company'
+        string="Company",
     )
-    date_range_id = fields.Many2one(
-        comodel_name='date.range',
-        string='Date range'
+    date_range_id = fields.Many2one(comodel_name="date.range", string="Date range")
+    date_from = fields.Date(required=True, default=lambda self: self._init_date_from())
+    date_to = fields.Date(required=True, default=fields.Date.context_today)
+    fy_start_date = fields.Date(compute="_compute_fy_start_date")
+    target_move = fields.Selection(
+        [("posted", "All Posted Entries"), ("all", "All Entries")],
+        string="Target Moves",
+        required=True,
+        default="all",
     )
-    date_from = fields.Date(required=True,
-                            default=lambda self: self._init_date_from())
-    date_to = fields.Date(required=True,
-                          default=fields.Date.context_today)
-    fy_start_date = fields.Date(compute='_compute_fy_start_date')
-    target_move = fields.Selection([('posted', 'All Posted Entries'),
-                                    ('all', 'All Entries')],
-                                   string='Target Moves',
-                                   required=True,
-                                   default='all')
     account_ids = fields.Many2many(
-        comodel_name='account.account',
-        string='Filter accounts',
+        comodel_name="account.account", string="Filter accounts",
     )
-    centralize = fields.Boolean(string='Activate centralization',
-                                default=True)
+    centralize = fields.Boolean(string="Activate centralization", default=True)
     hide_account_at_0 = fields.Boolean(
-        string='Hide account ending balance at 0',
-        help='Use this filter to hide an account or a partner '
-             'with an ending balance at 0. '
-             'If partners are filtered, '
-             'debits and credits totals will not match the trial balance.'
+        string="Hide account ending balance at 0",
+        help="Use this filter to hide an account or a partner "
+        "with an ending balance at 0. "
+        "If partners are filtered, "
+        "debits and credits totals will not match the trial balance.",
     )
-    show_analytic_tags = fields.Boolean(
-        string='Show analytic tags',
-    )
+    show_analytic_tags = fields.Boolean(string="Show analytic tags",)
     receivable_accounts_only = fields.Boolean()
     payable_accounts_only = fields.Boolean()
     partner_ids = fields.Many2many(
-        comodel_name='res.partner',
-        string='Filter partners',
+        comodel_name="res.partner",
+        string="Filter partners",
         default=lambda self: self._default_partners(),
     )
     analytic_tag_ids = fields.Many2many(
-        comodel_name='account.analytic.tag',
-        string='Filter analytic tags',
+        comodel_name="account.analytic.tag", string="Filter analytic tags",
     )
     account_journal_ids = fields.Many2many(
-        comodel_name='account.journal',
-        string='Filter journals',
+        comodel_name="account.journal", string="Filter journals",
     )
     cost_center_ids = fields.Many2many(
-        comodel_name='account.analytic.account',
-        string='Filter cost centers',
+        comodel_name="account.analytic.account", string="Filter cost centers",
     )
 
     not_only_one_unaffected_earnings_account = fields.Boolean(
-        readonly=True,
-        string='Not only one unaffected earnings account'
+        readonly=True, string="Not only one unaffected earnings account"
     )
     foreign_currency = fields.Boolean(
-        string='Show foreign currency',
-        help='Display foreign currency for move lines, unless '
-             'account currency is not setup through chart of accounts '
-             'will display initial and final balance in that currency.',
+        string="Show foreign currency",
+        help="Display foreign currency for move lines, unless "
+        "account currency is not setup through chart of accounts "
+        "will display initial and final balance in that currency.",
         default=lambda self: self._default_foreign_currency(),
     )
 
@@ -95,73 +84,87 @@ class GeneralLedgerReportWizard(models.TransientModel):
         last_fsc_month = self.env.user.company_id.fiscalyear_last_month
         last_fsc_day = self.env.user.company_id.fiscalyear_last_day
 
-        if cur_month < last_fsc_month \
-                or cur_month == last_fsc_month and cur_day <= last_fsc_day:
-            return time.strftime('%Y-01-01')
+        if (
+            cur_month < last_fsc_month
+            or cur_month == last_fsc_month
+            and cur_day <= last_fsc_day
+        ):
+            return time.strftime("%Y-01-01")
 
     def _default_foreign_currency(self):
-        return self.env.user.has_group('base.group_multi_currency')
+        return self.env.user.has_group("base.group_multi_currency")
 
-    @api.depends('date_from')
+    @api.depends("date_from")
     def _compute_fy_start_date(self):
-        for wiz in self.filtered('date_from'):
+        for wiz in self.filtered("date_from"):
             date = fields.Datetime.from_string(wiz.date_from)
             res = self.company_id.compute_fiscalyear_dates(date)
-            wiz.fy_start_date = fields.Date.to_string(res['date_from'])
+            wiz.fy_start_date = fields.Date.to_string(res["date_from"])
 
-    @api.onchange('company_id')
+    @api.onchange("company_id")
     def onchange_company_id(self):
         """Handle company change."""
-        account_type = self.env.ref('account.data_unaffected_earnings')
-        count = self.env['account.account'].search_count(
+        account_type = self.env.ref("account.data_unaffected_earnings")
+        count = self.env["account.account"].search_count(
             [
-                ('user_type_id', '=', account_type.id),
-                ('company_id', '=', self.company_id.id)
-            ])
+                ("user_type_id", "=", account_type.id),
+                ("company_id", "=", self.company_id.id),
+            ]
+        )
         self.not_only_one_unaffected_earnings_account = count != 1
-        if self.company_id and self.date_range_id.company_id and \
-                self.date_range_id.company_id != self.company_id:
+        if (
+            self.company_id
+            and self.date_range_id.company_id
+            and self.date_range_id.company_id != self.company_id
+        ):
             self.date_range_id = False
         if self.company_id and self.account_journal_ids:
             self.account_journal_ids = self.account_journal_ids.filtered(
-                lambda p: p.company_id == self.company_id or
-                not p.company_id)
+                lambda p: p.company_id == self.company_id or not p.company_id
+            )
         if self.company_id and self.partner_ids:
             self.partner_ids = self.partner_ids.filtered(
-                lambda p: p.company_id == self.company_id or
-                not p.company_id)
+                lambda p: p.company_id == self.company_id or not p.company_id
+            )
         if self.company_id and self.account_ids:
             if self.receivable_accounts_only or self.payable_accounts_only:
                 self.onchange_type_accounts_only()
             else:
                 self.account_ids = self.account_ids.filtered(
-                    lambda a: a.company_id == self.company_id)
+                    lambda a: a.company_id == self.company_id
+                )
         if self.company_id and self.cost_center_ids:
             self.cost_center_ids = self.cost_center_ids.filtered(
-                lambda c: c.company_id == self.company_id)
-        res = {'domain': {'account_ids': [],
-                          'partner_ids': [],
-                          'account_journal_ids': [],
-                          'cost_center_ids': [],
-                          'date_range_id': []
-                          }
-               }
+                lambda c: c.company_id == self.company_id
+            )
+        res = {
+            "domain": {
+                "account_ids": [],
+                "partner_ids": [],
+                "account_journal_ids": [],
+                "cost_center_ids": [],
+                "date_range_id": [],
+            }
+        }
         if not self.company_id:
             return res
         else:
-            res['domain']['account_ids'] += [
-                ('company_id', '=', self.company_id.id)]
-            res['domain']['account_journal_ids'] += [
-                ('company_id', '=', self.company_id.id)]
-            res['domain']['partner_ids'] += self._get_partner_ids_domain()
-            res['domain']['cost_center_ids'] += [
-                ('company_id', '=', self.company_id.id)]
-            res['domain']['date_range_id'] += [
-                '|', ('company_id', '=', self.company_id.id),
-                ('company_id', '=', False)]
+            res["domain"]["account_ids"] += [("company_id", "=", self.company_id.id)]
+            res["domain"]["account_journal_ids"] += [
+                ("company_id", "=", self.company_id.id)
+            ]
+            res["domain"]["partner_ids"] += self._get_partner_ids_domain()
+            res["domain"]["cost_center_ids"] += [
+                ("company_id", "=", self.company_id.id)
+            ]
+            res["domain"]["date_range_id"] += [
+                "|",
+                ("company_id", "=", self.company_id.id),
+                ("company_id", "=", False),
+            ]
         return res
 
-    @api.onchange('date_range_id')
+    @api.onchange("date_range_id")
     def onchange_date_range_id(self):
         """Handle date range change."""
         if self.date_range_id:
@@ -169,31 +172,37 @@ class GeneralLedgerReportWizard(models.TransientModel):
             self.date_to = self.date_range_id.date_end
 
     @api.multi
-    @api.constrains('company_id', 'date_range_id')
+    @api.constrains("company_id", "date_range_id")
     def _check_company_id_date_range_id(self):
         for rec in self.sudo():
-            if rec.company_id and rec.date_range_id.company_id and\
-                    rec.company_id != rec.date_range_id.company_id:
+            if (
+                rec.company_id
+                and rec.date_range_id.company_id
+                and rec.company_id != rec.date_range_id.company_id
+            ):
                 raise ValidationError(
-                    _('The Company in the General Ledger Report Wizard and in '
-                      'Date Range must be the same.'))
+                    _(
+                        "The Company in the General Ledger Report Wizard and in "
+                        "Date Range must be the same."
+                    )
+                )
 
-    @api.onchange('receivable_accounts_only', 'payable_accounts_only')
+    @api.onchange("receivable_accounts_only", "payable_accounts_only")
     def onchange_type_accounts_only(self):
         """Handle receivable/payable accounts only change."""
         if self.receivable_accounts_only or self.payable_accounts_only:
-            domain = [('company_id', '=', self.company_id.id)]
+            domain = [("company_id", "=", self.company_id.id)]
             if self.receivable_accounts_only and self.payable_accounts_only:
-                domain += [('internal_type', 'in', ('receivable', 'payable'))]
+                domain += [("internal_type", "in", ("receivable", "payable"))]
             elif self.receivable_accounts_only:
-                domain += [('internal_type', '=', 'receivable')]
+                domain += [("internal_type", "=", "receivable")]
             elif self.payable_accounts_only:
-                domain += [('internal_type', '=', 'payable')]
-            self.account_ids = self.env['account.account'].search(domain)
+                domain += [("internal_type", "=", "payable")]
+            self.account_ids = self.env["account.account"].search(domain)
         else:
             self.account_ids = None
 
-    @api.onchange('partner_ids')
+    @api.onchange("partner_ids")
     def onchange_partner_ids(self):
         """Handle partners change."""
         if self.partner_ids:
@@ -202,73 +211,77 @@ class GeneralLedgerReportWizard(models.TransientModel):
             self.receivable_accounts_only = self.payable_accounts_only = False
 
     @api.multi
-    @api.depends('company_id')
+    @api.depends("company_id")
     def _compute_unaffected_earnings_account(self):
-        account_type = self.env.ref('account.data_unaffected_earnings')
+        account_type = self.env.ref("account.data_unaffected_earnings")
         for record in self:
-            record.unaffected_earnings_account = self.env[
-                'account.account'].search(
+            record.unaffected_earnings_account = self.env["account.account"].search(
                 [
-                    ('user_type_id', '=', account_type.id),
-                    ('company_id', '=', record.company_id.id)
-                ])
+                    ("user_type_id", "=", account_type.id),
+                    ("company_id", "=", record.company_id.id),
+                ]
+            )
 
     unaffected_earnings_account = fields.Many2one(
-        comodel_name='account.account',
-        compute='_compute_unaffected_earnings_account',
-        store=True
+        comodel_name="account.account",
+        compute="_compute_unaffected_earnings_account",
+        store=True,
     )
 
     @api.multi
     def _print_report(self, report_type):
         self.ensure_one()
         data = self._prepare_report_general_ledger()
-        if report_type == 'xlsx':
-            report_name = 'a_f_r.report_general_ledger_xlsx'
+        if report_type == "xlsx":
+            report_name = "a_f_r.report_general_ledger_xlsx"
         else:
-            report_name = 'account_financial_report.general_ledger'
-        return self.env['ir.actions.report'].search(
-            [('report_name', '=', report_name),
-             ('report_type', '=', report_type)], limit=1).report_action(
-            self, data=data)
+            report_name = "account_financial_report.general_ledger"
+        return (
+            self.env["ir.actions.report"]
+            .search(
+                [("report_name", "=", report_name), ("report_type", "=", report_type)],
+                limit=1,
+            )
+            .report_action(self, data=data)
+        )
 
     @api.multi
     def button_export_html(self):
         self.ensure_one()
-        report_type = 'qweb-html'
+        report_type = "qweb-html"
         return self._export(report_type)
 
     @api.multi
     def button_export_pdf(self):
         self.ensure_one()
-        report_type = 'qweb-pdf'
+        report_type = "qweb-pdf"
         return self._export(report_type)
 
     @api.multi
     def button_export_xlsx(self):
         self.ensure_one()
-        report_type = 'xlsx'
+        report_type = "xlsx"
         return self._export(report_type)
 
     def _prepare_report_general_ledger(self):
         self.ensure_one()
         return {
-            'wizard_id': self.id,
-            'date_from': self.date_from,
-            'date_to': self.date_to,
-            'only_posted_moves': self.target_move == 'posted',
-            'hide_account_at_0': self.hide_account_at_0,
-            'foreign_currency': self.foreign_currency,
-            'show_analytic_tags': self.show_analytic_tags,
-            'company_id': self.company_id.id,
-            'account_ids': self.account_ids.ids,
-            'partner_ids': self.partner_ids.ids,
-            'cost_center_ids': self.cost_center_ids.ids,
-            'analytic_tag_ids': self.analytic_tag_ids.ids,
-            'journal_ids': self.account_journal_ids.ids,
-            'centralize': self.centralize,
-            'fy_start_date': self.fy_start_date,
-            'unaffected_earnings_account': self.unaffected_earnings_account.id,
+            "wizard_id": self.id,
+            "date_from": self.date_from,
+            "date_to": self.date_to,
+            "only_posted_moves": self.target_move == "posted",
+            "hide_account_at_0": self.hide_account_at_0,
+            "foreign_currency": self.foreign_currency,
+            "show_analytic_tags": self.show_analytic_tags,
+            "company_id": self.company_id.id,
+            "account_ids": self.account_ids.ids,
+            "partner_ids": self.partner_ids.ids,
+            "cost_center_ids": self.cost_center_ids.ids,
+            "analytic_tag_ids": self.analytic_tag_ids.ids,
+            "journal_ids": self.account_journal_ids.ids,
+            "centralize": self.centralize,
+            "fy_start_date": self.fy_start_date,
+            "unaffected_earnings_account": self.unaffected_earnings_account.id,
         }
 
     def _export(self, report_type):
