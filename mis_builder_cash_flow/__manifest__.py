a96ec4e1dd81b1e98ea25fb96f3a9b21d565b5d6
mreficent <miquel.raich@eficent.com>
2020-04-23 15:19:00 +0200

    [IMP] mis_builder_cash_flow: black, isort, prettier

--- mis_builder_cash_flow/__manifest__.py
+++ mis_builder_cash_flow/__manifest__.py
@@ -2,25 +2,22 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'MIS Builder Cash Flow',
-    'version': '12.0.1.3.0',
-    'license': 'LGPL-3',
-    'author': 'ADHOC SA, '
-              'Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/mis-builder',
-    'depends': [
-        'mis_builder',
+    "name": "MIS Builder Cash Flow",
+    "version": "12.0.1.3.0",
+    "license": "LGPL-3",
+    "author": "ADHOC SA, " "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/mis-builder",
+    "depends": ["mis_builder",],
+    "data": [
+        "security/mis_cash_flow_security.xml",
+        "report/mis_cash_flow_views.xml",
+        "views/mis_cash_flow_forecast_line_views.xml",
+        "views/account_account_views.xml",
+        "data/mis_report_style.xml",
+        "data/mis_report.xml",
+        "data/mis_report_instance.xml",
     ],
-    'data': [
-        'security/mis_cash_flow_security.xml',
-        'report/mis_cash_flow_views.xml',
-        'views/mis_cash_flow_forecast_line_views.xml',
-        'views/account_account_views.xml',
-        'data/mis_report_style.xml',
-        'data/mis_report.xml',
-        'data/mis_report_instance.xml',
-    ],
-    'installable': True,
-    'maintainers': ['jjscarafia'],
-    'development_status': 'Beta',
+    "installable": True,
+    "maintainers": ["jjscarafia"],
+    "development_status": "Beta",
 }
--- mis_builder_cash_flow/report/mis_cash_flow.py
+++ mis_builder_cash_flow/report/mis_cash_flow.py
@@ -1,79 +1,61 @@
 # Copyright 2019 ADHOC SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from odoo import api, fields, models, tools
 from psycopg2.extensions import AsIs
+
+from odoo import api, fields, models, tools
 
 
 class MisCashFlow(models.Model):
 
-    _name = 'mis.cash_flow'
-    _description = 'MIS Cash Flow'
+    _name = "mis.cash_flow"
+    _description = "MIS Cash Flow"
     _auto = False
 
     line_type = fields.Selection(
-        [('forecast_line', 'Forecast Line'), ('move_line', 'Journal Item')],
+        [("forecast_line", "Forecast Line"), ("move_line", "Journal Item")],
         index=True,
         readonly=True,
     )
-    name = fields.Char(
-        readonly=True,
-    )
+    name = fields.Char(readonly=True,)
     account_id = fields.Many2one(
-        comodel_name='account.account',
-        string='Account',
+        comodel_name="account.account",
+        string="Account",
         auto_join=True,
         index=True,
         readonly=True,
     )
     partner_id = fields.Many2one(
-        comodel_name='res.partner',
-        string='Partner',
-        readonly=True,
+        comodel_name="res.partner", string="Partner", readonly=True,
     )
     move_line_id = fields.Many2one(
-        comodel_name='account.move.line',
-        string='Journal Item',
+        comodel_name="account.move.line",
+        string="Journal Item",
         auto_join=True,
         readonly=True,
     )
     company_id = fields.Many2one(
-        comodel_name='res.company',
-        string='Company',
+        comodel_name="res.company",
+        string="Company",
         auto_join=True,
         readonly=True,
         index=True,
     )
-    credit = fields.Float(
-        readonly=True,
-    )
-    debit = fields.Float(
-        readonly=True,
-    )
-    date = fields.Date(
-        readonly=True,
-        index=True,
-    )
-    reconciled = fields.Boolean(
-        readonly=True,
-    )
+    credit = fields.Float(readonly=True,)
+    debit = fields.Float(readonly=True,)
+    date = fields.Date(readonly=True, index=True,)
+    reconciled = fields.Boolean(readonly=True,)
     full_reconcile_id = fields.Many2one(
-        'account.full.reconcile',
-        string="Matching Number",
-        readonly=True,
-        index=True,
+        "account.full.reconcile", string="Matching Number", readonly=True, index=True,
     )
     user_type_id = fields.Many2one(
-        'account.account.type',
-        auto_join=True,
-        readonly=True,
-        index=True,
+        "account.account.type", auto_join=True, readonly=True, index=True,
     )
 
     @api.model_cr
     def init(self):
-        account_type_receivable = self.env.ref(
-            'account.data_account_type_receivable')
-        query = """
+        account_type_receivable = self.env.ref("account.data_account_type_receivable")
+        query = (
+            """
             SELECT
                 -- we use negative id to avoid duplicates and we don't use
                 -- ROW_NUMBER() because the performance was very poor
@@ -123,18 +105,22 @@ class MisCashFlow(models.Model):
                 fl.name as name,
                 fl.date as date
             FROM mis_cash_flow_forecast_line as fl
-        """ % account_type_receivable.id
+        """
+            % account_type_receivable.id
+        )
         tools.drop_view_if_exists(self.env.cr, self._table)
         self._cr.execute(
-            'CREATE OR REPLACE VIEW %s AS %s',
-            (AsIs(self._table), AsIs(query))
+            "CREATE OR REPLACE VIEW %s AS %s", (AsIs(self._table), AsIs(query))
         )
 
     @api.multi
     def action_open_related_line(self):
         self.ensure_one()
-        if self.line_type == 'move_line':
+        if self.line_type == "move_line":
             return self.move_line_id.get_formview_action()
         else:
-            return self.env['mis.cash_flow.forecast_line'].browse(
-                self.id).get_formview_action()
+            return (
+                self.env["mis.cash_flow.forecast_line"]
+                .browse(self.id)
+                .get_formview_action()
+            )
