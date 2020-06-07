44bd1893ea1a1b3dbba7d4db983a0544dedcaabd
Alexis de Lattre <alexis.delattre@akretion.com>
2020-06-07 15:52:43 +0200

    [MIG] account_bank_reconciliation_summary_xlsx to v13

--- account_bank_reconciliation_summary_xlsx/__manifest__.py
+++ account_bank_reconciliation_summary_xlsx/__manifest__.py
@@ -1,10 +1,10 @@
-# Copyright 2017-2019 Akretion France (http://www.akretion.com/)
+# Copyright 2017-2020 Akretion France (http://www.akretion.com/)
 # @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Bank Reconciliation Report',
-    'version': '12.0.1.0.0',
+    'version': '13.0.1.0.0',
     'license': 'AGPL-3',
     'author': "Akretion,Odoo Community Association (OCA)",
     'website': 'https://github.com/OCA/account-financial-reporting',
