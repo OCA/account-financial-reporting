26ae804b6aa375963fd66ec6ab004ea35f12efe7
Joan Sisquella <joan.sisquella@forgeflow.com>
2020-09-04 09:54:26 +0200

    [FIX] General ledger XLSX: solved mistake in the info

--- account_financial_report/__manifest__.py
+++ account_financial_report/__manifest__.py
@@ -5,7 +5,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Account Financial Reports",
-    "version": "13.0.1.3.2",
+    "version": "13.0.1.4.0",
     "category": "Reporting",
     "summary": "OCA Financial Reports",
     "author": "Camptocamp SA,"
