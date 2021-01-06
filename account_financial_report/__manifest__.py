cc9ea42a519a2fcf0cd9964838999e217a8e39d6
Stefan Rijnhart <stefan@opener.am>
2021-01-06 15:03:15 +0100

    [UPD] Update account_financial_report.pot
    [a0cb11bb] [FIX] Journal ledger wizard to inherit from abstract report wizard
    
    Fixes
        AttributeError: type object 'journal.ledger.report.wizard' has no attribute
        'button_export_xlsx'
    
    Regression of #735

--- account_financial_report/__manifest__.py
+++ account_financial_report/__manifest__.py
@@ -6,7 +6,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Account Financial Reports",
-    "version": "14.0.1.5.1",
+    "version": "14.0.2.0.0",
     "category": "Reporting",
     "summary": "OCA Financial Reports",
     "author": "Camptocamp SA,"
