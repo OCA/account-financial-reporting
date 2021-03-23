f19ce93542353950bf257a4264c07b1d985e1f94
Alexandre D. Díaz <alexandre.diaz@tecnativa.com>
2021-03-23 14:26:10 +0100

    [FIX] account_financial_report: Actions

---
+++ account_financial_report/static/src/js/action_manager_report.js
@@ -0,0 +1,35 @@
+odoo.define("account_financial_report.ReportActionManager", function (require) {
+    "use strict";
+
+    const ActionManager = require("web.ActionManager");
+    require("web.ReportActionManager");
+
+    ActionManager.include({
+        /**
+         * @override
+         */
+        _executeReportClientAction: function (action, options) {
+            const MODULE_NAME = "account_financial_report";
+
+            // When 'report_action' is called from the backend, Odoo hardcodes the action tag.
+            // We have to make a hack to use our own report controller.
+            if (action.report_name.startsWith(`${MODULE_NAME}.`)) {
+                const urls = this._makeReportUrls(action);
+                const clientActionOptions = _.extend({}, options, {
+                    context: action.context,
+                    data: action.data,
+                    display_name: action.display_name,
+                    name: action.name,
+                    report_file: action.report_file,
+                    report_name: action.report_name,
+                    report_url: urls.html,
+                });
+                return this.doAction(
+                    "account_financial_report.client_action",
+                    clientActionOptions
+                );
+            }
+            return this._super.apply(this, arguments);
+        },
+    });
+});
