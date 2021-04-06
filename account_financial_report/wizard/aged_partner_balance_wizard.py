d542cd078b87c90bdace8776a827e3f8d8faedd7
Cas Vissers <cas.vissers@360erp.nl>
2021-04-06 16:15:05 +0200

    [FIX] Default company should not be used since it doesn't mutate when switching companies.

--- account_financial_report/wizard/aged_partner_balance_wizard.py
+++ account_financial_report/wizard/aged_partner_balance_wizard.py
@@ -15,7 +15,7 @@ class AgedPartnerBalanceWizard(models.TransientModel):
 
     company_id = fields.Many2one(
         comodel_name="res.company",
-        default=lambda self: self.env.user.company_id,
+        default=lambda self: self.env.company,
         required=False,
         string="Company",
     )
