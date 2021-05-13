71ef18dd08443c7757719f35508ba3c237d54aa6
João Marques <joao.marques@tecnativa.com>
2021-05-13 09:06:26 +0100

    [FIX] account_financial_report: filter by correct data in trial balance
    When on a Trial Balance report and filtering by hierarchical accounts or for a foreing currency, an error was produced when generating the report.
    
    The report fields were dependant on data that is no longer present, such as line data.
    
    This uses the correct data provided when generating the report to avoid this errors
    
    TT29261

--- account_financial_report/__manifest__.py
+++ account_financial_report/__manifest__.py
@@ -6,7 +6,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Account Financial Reports",
-    "version": "14.0.1.2.1",
+    "version": "14.0.1.2.3",
     "category": "Reporting",
     "summary": "OCA Financial Reports",
     "author": "Camptocamp SA,"
