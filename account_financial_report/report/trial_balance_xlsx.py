20d6b67ce338a6dc8d273d74f2adcde1b04d76e9
Jordi Ballester <jordi.ballester@forgeflow.com>
2021-06-12 11:41:45 +0200

    [account_financial_report][fix] display correct target_move in Trial Balance XLSX

--- account_financial_report/report/trial_balance_xlsx.py
+++ account_financial_report/report/trial_balance_xlsx.py
@@ -148,7 +148,7 @@ class TrialBalanceXslx(models.AbstractModel):
             [
                 _("Target moves filter"),
                 _("All posted entries")
-                if report.target_move == "all"
+                if report.target_move == "posted"
                 else _("All entries"),
             ],
             [
