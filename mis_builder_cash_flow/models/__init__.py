e2fc912330880ca83f251b4b979d87ae5170c5fe
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2021-07-07 18:24:47 +0200

    [IMP] mis_builder_cash_flow: Consider states according selection
    "Target Moves" option in MIS report instance was previously ignored.
    
    Now it's taken into account, but still excluding cancelled entries.

--- mis_builder_cash_flow/models/__init__.py
+++ mis_builder_cash_flow/models/__init__.py
@@ -1,4 +1,4 @@
-# Copyright 2019 ADHOC SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from . import mis_cash_flow_forecast_line
 from . import account_account
+from . import mis_cash_flow_forecast_line
+from . import mis_report_instance
