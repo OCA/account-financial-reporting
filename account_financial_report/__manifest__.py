12af2f1ad5aa93983f9d2d48ac38df2f345de275
Abraham Anes <abraham@studio73.es>
2021-09-15 17:33:46 +0200

    [14.0][FIX] account_financial_reporting: wrong view_id

--- account_financial_report/__manifest__.py
+++ account_financial_report/__manifest__.py
@@ -6,7 +6,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Account Financial Reports",
-    "version": "14.0.1.3.2",
+    "version": "14.0.1.4.0",
     "category": "Reporting",
     "summary": "OCA Financial Reports",
     "author": "Camptocamp SA,"
