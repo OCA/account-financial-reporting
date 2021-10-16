19b47968de9c68b58c415ac0de6fcc129e0d493f
Raphaël Valyi <rvalyi@akretion.com>
2021-10-16 18:16:19 -0300

    [MIG] mis_builder_cash_flow: Migration to 14.0

--- mis_builder_cash_flow/__manifest__.py
+++ mis_builder_cash_flow/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "MIS Builder Cash Flow",
-    "version": "13.0.1.1.0",
+    "version": "14.0.1.0.0",
     "license": "LGPL-3",
     "author": "ADHOC SA, " "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/account-financial-reporting",
