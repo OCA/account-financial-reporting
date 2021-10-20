8cfc7dc8e0fb48dd5d3704e5856234406456408f
Luis Escobar <lescobar@vauxoo.com>
2021-10-20 21:19:53 +0000

    [FIX] account_tax_balance: Fixed translation of the financial type field values

--- account_tax_balance/__manifest__.py
+++ account_tax_balance/__manifest__.py
@@ -6,7 +6,7 @@
 {
     "name": "Tax Balance",
     "summary": "Compute tax balances based on date range",
-    "version": "14.0.1.1.1",
+    "version": "14.0.1.2.0",
     "development_status": "Production/Stable",
     "category": "Invoices & Payments",
     "website": "https://github.com/OCA/account-financial-reporting",
