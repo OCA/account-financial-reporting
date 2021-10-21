85f23bcc446e9bc92c07afc3d7c6fe6044038a32
tafaRU <alex.comba@agilebg.com>
2021-10-21 11:27:15 +0200

    account_tax_balance: change development_status to Production/Stable

--- account_tax_balance/__manifest__.py
+++ account_tax_balance/__manifest__.py
@@ -7,6 +7,7 @@
     "name": "Tax Balance",
     "summary": "Compute tax balances based on date range",
     "version": "14.0.1.1.0",
+    "development_status": "Production/Stable",
     "category": "Invoices & Payments",
     "website": "https://github.com/OCA/account-financial-reporting",
     "author": "Agile Business Group, Therp BV, Tecnativa, ACSONE SA/NV, "
