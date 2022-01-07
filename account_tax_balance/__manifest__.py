27fbba103dd50e39d172891381eee745c619e59c
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2022-01-07 08:25:00 +0100

    [IMP] account_tax_balance: Promote to mature
    It's there for a lot of versions, and with enough test coverage.

--- account_tax_balance/__manifest__.py
+++ account_tax_balance/__manifest__.py
@@ -1,13 +1,13 @@
 # Copyright 2016 Lorenzo Battistini - Agile Business Group
 # Copyright 2016 Tecnativa - Antonio Espinosa
 # Copyright 2016 ACSONE SA/NV - Stéphane Bidoul
-# Copyright 2017 Tecnativa - Pedro M. Baeza
+# Copyright 2017-2021 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Tax Balance",
     "summary": "Compute tax balances based on date range",
     "version": "14.0.1.2.1",
-    "development_status": "Production/Stable",
+    "development_status": "Mature",
     "category": "Invoices & Payments",
     "website": "https://github.com/OCA/account-financial-reporting",
     "author": "Agile Business Group, Therp BV, Tecnativa, ACSONE SA/NV, "
@@ -23,4 +23,5 @@
     ],
     "images": ["images/tax_balance.png"],
     "pre_init_hook": "pre_init_hook",
+    "maintainers": ["pedrobaeza"],
 }
