acae7e272c3e0528b125062b121cd10c65419f20
Le Dinh Tien <15520880@gm.uit.edu.vn>
2022-10-10 10:08:02 +0700

    [MIG] mis_template_financial_report: Migration to 15.0

--- mis_template_financial_report/__manifest__.py
+++ mis_template_financial_report/__manifest__.py
@@ -6,7 +6,7 @@
     "version": "15.0.1.0.0",
     "license": "AGPL-3",
     "author": "Hunki Enterprises BV,Odoo Community Association (OCA)",
-    "website": "https://github.com/OCA/mis-builder",
+    "website": "https://github.com/OCA/account-financial-reporting",
     "category": "Localization",
     "depends": ["mis_builder"],
     "data": [
