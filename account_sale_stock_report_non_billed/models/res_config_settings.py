54df2055d5a96b8eb147fcfe20e558a24e249bfc
CarlosRoca13 <carlos.roca@tecnativa.com>
2022-11-14 11:09:52 +0100

    [IMP] account_*_stock_report_non_billed: Allow use dates as a range
    By doing this change we can use dates (threshold and check) as a range
    to discart moves and invoices that are out of this range.
    
    TT40443

--- account_sale_stock_report_non_billed/models/res_config_settings.py
+++ account_sale_stock_report_non_billed/models/res_config_settings.py
@@ -10,3 +10,7 @@ class ResConfigSettings(models.TransientModel):
     stock_move_non_billed_threshold = fields.Date(
         related="company_id.stock_move_non_billed_threshold", readonly=False
     )
+    default_interval_restrict_invoices = fields.Boolean(
+        string="Restrict invoices using the date interval",
+        default_model="account.sale.stock.report.non.billed.wiz",
+    )
