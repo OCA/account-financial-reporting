2a4b6ee093cae507c258dbd0fac0c062cdff660b
Jérémy Didderen <jeremy.didderen@noviat.com>
2023-01-13 21:21:29 +0100

    [MIG] account_move_line_report_xls: Migration to 15.0

--- account_move_line_report_xls/__manifest__.py
+++ account_move_line_report_xls/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Account Move Line XLSX export",
-    "version": "14.0.1.0.1",
+    "version": "15.0.1.0.0",
     "license": "AGPL-3",
     "author": "Noviat, Odoo Community Association (OCA)",
     "category": "Accounting & Finance",
