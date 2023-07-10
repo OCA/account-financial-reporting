3d8e764f6b2d8bdc15a70a22679d44920252fe70
sergiocorato <sergiocorato@gmail.com>
2023-07-10 15:24:35 +0200

    [FIX] license

--- mis_builder_cash_flow/__manifest__.py
+++ mis_builder_cash_flow/__manifest__.py
@@ -4,7 +4,7 @@
 {
     'name': 'MIS Builder Cash Flow',
     'version': '12.0.1.3.0',
-    'license': 'LGPL-3',
+    'license': 'AGPL-3',
     'author': 'ADHOC SA, '
               'Odoo Community Association (OCA)',
     'website': 'https://github.com/OCA/account-financial-reporting',
