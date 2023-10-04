7a7edf1ae120e85d01175970334b4ba7dd8f2048
Simone Rubino <simone.rubino@aion-tech.it>
2023-10-04 12:24:57 +0200

    [MIG] mis_template_financial_report: Migration to 16.0

--- mis_template_financial_report/__manifest__.py
+++ mis_template_financial_report/__manifest__.py
@@ -1,9 +1,10 @@
 # Copyright 2020 Hunki Enterprises BV
+# Copyright 2023 Simone Rubino - Aion Tech
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
     "name": "Profit & Loss / Balance sheet MIS templates",
-    "version": "15.0.1.0.1",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "author": "Hunki Enterprises BV,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/account-financial-reporting",
