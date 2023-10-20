c6c4dac24762cf5721d58d9147ef29b7de5e610c
Denis Roussel <denis.roussel@acsone.eu>
2023-10-20 11:11:50 +0200

    [IMP] account_financial_report: Don't store analytic_account_ids
    On account.move.line, don't store here the analytic accounts as it can be
    bulky (especially the init on big database).

--- account_financial_report/models/account_move_line.py
+++ account_financial_report/models/account_move_line.py
@@ -1,39 +1,10 @@
 # Copyright 2019 ACSONE SA/NV (<http://acsone.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).-
-from collections import defaultdict
-
-from odoo import api, fields, models
+from odoo import api, models
 
 
 class AccountMoveLine(models.Model):
     _inherit = "account.move.line"
-
-    analytic_account_ids = fields.Many2many(
-        "account.analytic.account", compute="_compute_analytic_account_ids", store=True
-    )
-
-    @api.depends("analytic_distribution")
-    def _compute_analytic_account_ids(self):
-        # Prefetch all involved analytic accounts
-        with_distribution = self.filtered("analytic_distribution")
-        batch_by_analytic_account = defaultdict(list)
-        for record in with_distribution:
-            for account_id in map(int, record.analytic_distribution):
-                batch_by_analytic_account[account_id].append(record.id)
-        existing_account_ids = set(
-            self.env["account.analytic.account"]
-            .browse(map(int, batch_by_analytic_account))
-            .exists()
-            .ids
-        )
-        # Store them
-        self.analytic_account_ids = False
-        for account_id, record_ids in batch_by_analytic_account.items():
-            if account_id not in existing_account_ids:
-                continue
-            self.browse(record_ids).analytic_account_ids = [
-                fields.Command.link(account_id)
-            ]
 
     def init(self):
         """
