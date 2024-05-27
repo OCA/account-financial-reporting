c9a5e7cb0b7ab4495ff316627d76d2e72b94dab1
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2024-05-27 20:22:23 +0200

    [OU-ADD] mis_builder_cash_flow: Migration scripts
    TT49378

---
+++ mis_builder_cash_flow/migrations/16.0.1.0.0/post-migration.py
@@ -0,0 +1,10 @@
+# Copyright 2024 Tecnativa - Pedro M. Baeza
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from openupgradelib import openupgrade
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    openupgrade.load_data(
+        env.cr, "mis_builder_cash_flow", "migrations/16.0.1.0.0/noupdate_changes.xml"
+    )
