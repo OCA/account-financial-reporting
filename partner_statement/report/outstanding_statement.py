ed25d5fa071fc9524682318a0f91e09b88df0c39
anderson <andersonvidal94@gmail.com>
2025-02-11 13:14:03 -0500

    [ADD] migrate to version 18

---
+++ partner_statement/report/outstanding_statement.py
@@ -0,0 +1,160 @@
+# Copyright 2018 ForgeFlow, S.L. (https://www.forgeflow.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from odoo import api, models
+from odoo.tools.float_utils import float_is_zero
+
+
+class OutstandingStatement(models.AbstractModel):
+    """Model of Outstanding Statement"""
+
+    _inherit = "statement.common"
+    _name = "report.partner_statement.outstanding_statement"
+    _description = "Partner Outstanding Statement"
+
+    def _display_outstanding_lines_sql_q1(self, partners, date_end, account_type):
+        partners = tuple(partners)
+        return str(
+            self._cr.mogrify(
+                """
+            SELECT l.id, m.name AS move_id, l.partner_id, l.date, l.name,
+                l.blocked, l.currency_id, l.company_id,
+            CASE WHEN l.ref IS NOT NULL
+                THEN l.ref
+                ELSE m.ref
+            END as ref,
+            CASE WHEN (l.currency_id is not null AND l.amount_currency > 0.0)
+                THEN avg(l.amount_currency)
+                ELSE avg(l.debit)
+            END as debit,
+            CASE WHEN (l.currency_id is not null AND l.amount_currency < 0.0)
+                THEN avg(l.amount_currency * (-1))
+                ELSE avg(l.credit)
+            END as credit,
+            CASE WHEN l.balance > 0.0
+                THEN l.balance - sum(coalesce(pd.amount, 0.0))
+                ELSE l.balance + sum(coalesce(pc.amount, 0.0))
+            END AS open_amount,
+            CASE WHEN l.balance > 0.0
+                THEN l.amount_currency - sum(coalesce(pd.debit_amount_currency, 0.0))
+                ELSE l.amount_currency + sum(coalesce(pc.credit_amount_currency, 0.0))
+            END AS open_amount_currency,
+            CASE WHEN l.date_maturity is null
+                THEN l.date
+                ELSE l.date_maturity
+            END as date_maturity
+            FROM account_move_line l
+            JOIN account_account aa ON (aa.id = l.account_id)
+            JOIN account_move m ON (l.move_id = m.id)
+            LEFT JOIN (SELECT pr.*
+                FROM account_partial_reconcile pr
+                INNER JOIN account_move_line l2
+                ON pr.credit_move_id = l2.id
+                WHERE l2.date <= %(date_end)s
+            ) as pd ON pd.debit_move_id = l.id
+            LEFT JOIN (SELECT pr.*
+                FROM account_partial_reconcile pr
+                INNER JOIN account_move_line l2
+                ON pr.debit_move_id = l2.id
+                WHERE l2.date <= %(date_end)s
+            ) as pc ON pc.credit_move_id = l.id
+            WHERE l.partner_id IN %(partners)s
+                AND (
+                    (pd.id IS NOT NULL AND
+                        pd.max_date <= %(date_end)s) OR
+                    (pc.id IS NOT NULL AND
+                        pc.max_date <= %(date_end)s) OR
+                    (pd.id IS NULL AND pc.id IS NULL)
+                ) AND l.date <= %(date_end)s AND m.state IN ('posted')
+                AND aa.account_type = %(account_type)s
+            GROUP BY l.id, l.partner_id, m.name, l.date, l.date_maturity, l.name,
+                CASE WHEN l.ref IS NOT NULL
+                    THEN l.ref
+                    ELSE m.ref
+                END,
+                l.blocked, l.currency_id, l.balance, l.amount_currency, l.company_id
+            """,
+                locals(),
+            ),
+            "utf-8",
+        )
+
+    def _display_outstanding_lines_sql_q2(self, sub):
+        return str(
+            self._cr.mogrify(
+                f"""
+                SELECT {sub}.partner_id, {sub}.currency_id, {sub}.move_id,
+                    {sub}.date, {sub}.date_maturity, {sub}.debit, {sub}.credit,
+                    {sub}.name, {sub}.ref, {sub}.blocked, {sub}.company_id,
+                    CASE WHEN {sub}.currency_id is not null
+                        THEN {sub}.open_amount_currency
+                        ELSE {sub}.open_amount
+                    END as open_amount, {sub}.id
+                FROM {sub}
+                """,
+                locals(),
+            ),
+            "utf-8",
+        )
+
+    def _display_outstanding_lines_sql_q3(self, sub, company_id):
+        return str(
+            self._cr.mogrify(
+                f"""
+            SELECT {sub}.partner_id, {sub}.move_id, {sub}.date,
+                {sub}.date_maturity, {sub}.name, {sub}.ref, {sub}.debit,
+                {sub}.credit, {sub}.debit-{sub}.credit AS amount,
+                COALESCE({sub}.currency_id, c.currency_id) AS currency_id,
+                {sub}.open_amount, {sub}.blocked, {sub}.id
+            FROM {sub}
+            JOIN res_company c ON (c.id = {sub}.company_id)
+            WHERE c.id = %(company_id)s AND {sub}.open_amount != 0.0
+            """,
+                locals(),
+            ),
+            "utf-8",
+        )
+
+    def _get_account_display_lines(
+        self, company_id, partner_ids, date_start, date_end, account_type
+    ):
+        res = dict(map(lambda x: (x, []), partner_ids))
+        partners = tuple(partner_ids)
+        # pylint: disable=E8103
+        self.env.cr.execute(
+            """
+        WITH Q1 as ({}),
+             Q2 AS ({}),
+             Q3 AS ({})
+        SELECT partner_id, currency_id, move_id, date, date_maturity, debit,
+            credit, amount, open_amount, COALESCE(name, '') as name,
+            COALESCE(ref, '') as ref, blocked, id
+        FROM Q3
+        ORDER BY date, date_maturity, move_id""".format(
+                self._display_outstanding_lines_sql_q1(
+                    partners, date_end, account_type
+                ),
+                self._display_outstanding_lines_sql_q2("Q1"),
+                self._display_outstanding_lines_sql_q3("Q2", company_id),
+            )
+        )
+        for row in self.env.cr.dictfetchall():
+            res[row.pop("partner_id")].append(row)
+        return res
+
+    def _add_currency_line(self, line, currency):
+        if float_is_zero(line["open_amount"], precision_rounding=currency.rounding):
+            return []
+        return [line]
+
+    @api.model
+    def _get_report_values(self, docids, data=None):
+        if not data:
+            data = {}
+        if "company_id" not in data:
+            wiz = self.env["outstanding.statement.wizard"].with_context(
+                active_ids=docids, model="res.partner"
+            )
+            data.update(wiz.create({})._prepare_statement())
+        data["amount_field"] = "open_amount"
+        return super()._get_report_values(docids, data)
---
+++ partner_statement/wizard/statement_common.py
@@ -0,0 +1,77 @@
+# Copyright 2018 Graeme Gellatly
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from dateutil.relativedelta import relativedelta
+
+from odoo import api, fields, models
+
+
+class StatementCommon(models.AbstractModel):
+    _name = "statement.common.wizard"
+    _description = "Statement Reports Common Wizard"
+
+    name = fields.Char()
+    company_id = fields.Many2one(
+        comodel_name="res.company",
+        default=lambda self: self.env.company,
+        string="Company",
+        required=True,
+    )
+    date_end = fields.Date(required=True, default=fields.Date.context_today)
+    show_aging_buckets = fields.Boolean(default=True)
+    number_partner_ids = fields.Integer(
+        default=lambda self: len(self._context["active_ids"])
+    )
+    filter_partners_non_due = fields.Boolean(
+        string="Don't show partners with no due entries", default=True
+    )
+    filter_negative_balances = fields.Boolean("Exclude Negative Balances", default=True)
+
+    aging_type = fields.Selection(
+        [("days", "Age by Days"), ("months", "Age by Months")],
+        string="Aging Method",
+        default="days",
+        required=True,
+    )
+
+    account_type = fields.Selection(
+        [("asset_receivable", "Receivable"), ("liability_payable", "Payable")],
+        default="asset_receivable",
+    )
+
+    @api.onchange("aging_type")
+    def onchange_aging_type(self):
+        if self.aging_type == "months":
+            self.date_end = fields.Date.context_today(self).replace(
+                day=1
+            ) - relativedelta(days=1)
+        else:
+            self.date_end = fields.Date.context_today(self)
+
+    def _prepare_statement(self):
+        self.ensure_one()
+        return {
+            "date_end": self.date_end,
+            "company_id": self.company_id.id,
+            "partner_ids": self._context["active_ids"],
+            "show_aging_buckets": self.show_aging_buckets,
+            "filter_non_due_partners": self.filter_partners_non_due,
+            "account_type": self.account_type,
+            "aging_type": self.aging_type,
+            "filter_negative_balances": self.filter_negative_balances,
+        }
+
+    def button_export_html(self):
+        self.ensure_one()
+        report_type = "qweb-html"
+        return self._export(report_type)
+
+    def button_export_pdf(self):
+        self.ensure_one()
+        report_type = "qweb-pdf"
+        return self._export(report_type)
+
+    def button_export_xlsx(self):
+        self.ensure_one()
+        report_type = "xlsx"
+        return self._export(report_type)
