4a4869d0726a7639b2ed887f05a613d800de5d50
Geraldo Lopez <geraldolopez@Geraldos-MacBook-Pro.local>
2025-05-20 23:49:28 -0700

    changing versions

---
+++ mis_builder_cash_flow/models/mis_cash_flow_forecast_line.py
@@ -0,0 +1,34 @@
+# Copyright 2019 ADHOC SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from odoo import _, api, fields, models
+from odoo.exceptions import ValidationError
+
+
+class MisCashFlowForecastLine(models.Model):
+    _name = "mis.cash_flow.forecast_line"
+    _description = "MIS Cash Flow Forecast Line"
+
+    date = fields.Date(required=True, index=True)
+    account_id = fields.Many2one(
+        comodel_name="account.account",
+        string="Account",
+        required=True,
+        help="The account of the forecast line is only for informative purpose",
+    )
+    partner_id = fields.Many2one(comodel_name="res.partner", string="Partner")
+    name = fields.Char(required=True, default="/")
+    balance = fields.Float(required=True)
+    company_id = fields.Many2one(
+        "res.company",
+        string="Company",
+        required=True,
+        default=lambda self: self.env.company,
+        index=True,
+    )
+
+    @api.constrains("company_id", "account_id")
+    def _check_company_id_account_id(self):
+        if self.filtered(lambda x: x.company_id != x.account_id.company_id):
+            raise ValidationError(
+                _("The Company and the Company of the Account must be the same.")
+            )
---
+++ mis_builder_cash_flow/tests/test_cash_flow.py
@@ -0,0 +1,150 @@
+# Copyright 2019 Creu Blanca
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from datetime import timedelta
+
+from odoo.exceptions import ValidationError
+from odoo.fields import Date
+from odoo.tests.common import TransactionCase, tagged
+from odoo.tools import mute_logger
+
+
+@tagged("post_install", "-at_install")
+class TestCashFlow(TransactionCase):
+    def setUp(self):
+        super().setUp()
+        self.company = self.env["res.company"].create({"name": "TEST"})
+        self.report = self.browse_ref("mis_builder_cash_flow.mis_instance_cash_flow")
+        self.report.company_id = self.company
+        self.bank_account = self.env["account.account"].create(
+            {
+                "company_id": self.company.id,
+                "code": "TEST1",
+                "name": "Bank account 01",
+                "account_type": "asset_cash",
+            }
+        )
+        self.bank_account_hide = self.env["account.account"].create(
+            {
+                "company_id": self.company.id,
+                "code": "TEST2",
+                "name": "Bank account 02",
+                "account_type": "asset_cash",
+                "hide_in_cash_flow": True,
+            }
+        )
+        self.account = self.env["account.account"].create(
+            {
+                "company_id": self.company.id,
+                "code": "TEST3",
+                "name": "Account",
+                "account_type": "asset_cash",
+                "reconcile": True,
+            }
+        )
+        self.journal = self.env["account.journal"].create(
+            {
+                "name": "Journal",
+                "code": "JOURNAL",
+                "company_id": self.company.id,
+                "type": "general",
+            }
+        )
+
+    def test_company_constrain(self):
+        with self.assertRaises(ValidationError):
+            self.env["mis.cash_flow.forecast_line"].create(
+                {"account_id": self.account.id, "date": Date.today(), "balance": 1000}
+            )
+
+    def test_report_instance(self):
+        self.check_matrix()
+        move = self.env["account.move"].create(
+            {
+                "name": "Move",
+                "journal_id": self.journal.id,
+                "company_id": self.company.id,
+                "move_type": "entry",
+                "line_ids": [
+                    (
+                        0,
+                        0,
+                        {
+                            "account_id": self.bank_account.id,
+                            "debit": 1500,
+                            "credit": 0,
+                            "company_id": self.company.id,
+                        },
+                    ),
+                    (
+                        0,
+                        0,
+                        {
+                            "account_id": self.bank_account_hide.id,
+                            "debit": 500,
+                            "credit": 0,
+                            "company_id": self.company.id,
+                        },
+                    ),
+                    (
+                        0,
+                        0,
+                        {
+                            "account_id": self.account.id,
+                            "debit": 0,
+                            "credit": 2000,
+                            "company_id": self.company.id,
+                        },
+                    ),
+                ],
+            }
+        )
+        move._post()
+        self.check_matrix(
+            args=[
+                ("liquidity", "Current", 1500),
+                ("balance", "Current", 1500),
+                ("in_receivable", "Current", -2000),
+            ],
+            ignore_rows=["balance", "period_balance", "in_total"],
+        )
+        date = Date.today() + timedelta(weeks=8)
+        self.env["mis.cash_flow.forecast_line"].create(
+            {
+                "account_id": self.account.id,
+                "date": date,
+                "balance": 1000,
+                "company_id": self.company.id,
+            }
+        )
+        self.check_matrix(
+            [
+                ("liquidity", "Current", 1500),
+                ("balance", "Current", 1500),
+                ("in_receivable", "Current", -2000),
+                ("in_forecast", "+8w", 1000),
+            ],
+            ignore_rows=["balance", "period_balance", "in_total"],
+        )
+
+    def check_matrix(self, args=None, ignore_rows=None):
+        if not args:
+            args = []
+        if not ignore_rows:
+            ignore_rows = []
+        with mute_logger("odoo.addons.mis_builder.models.kpimatrix"):
+            matrix = self.report._compute_matrix()
+        for row in matrix.iter_rows():
+            if row.kpi.name in ignore_rows:
+                continue
+            for cell in row.iter_cells():
+                if not cell:
+                    continue
+                found = False
+                label = cell.subcol.col.label
+                for exp in args:
+                    if exp[0] == row.kpi.name and exp[1] == label:
+                        found = True
+                        break
+                if not found:
+                    self.assertEqual(cell.val, 0)
