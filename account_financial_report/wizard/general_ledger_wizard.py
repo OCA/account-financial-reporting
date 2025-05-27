65972b2e0f4f6be01a58efc33bef93b6268a7999
Raul-S73 <raul@studio73.es>
2025-05-27 11:34:27 +0200

    [18.0][FIX] account_financial_report: Refact search and company_id field.

--- account_financial_report/wizard/general_ledger_wizard.py
+++ account_financial_report/wizard/general_ledger_wizard.py
@@ -103,13 +103,18 @@ class GeneralLedgerReportWizard(models.TransientModel):
         ):
             start_range = int(self.account_code_from.code)
             end_range = int(self.account_code_to.code)
-            self.account_ids = self.env["account.account"].search(
-                [("code", ">=", start_range), ("code", "<=", end_range)]
-            )
             if self.company_id:
-                self.account_ids = self.account_ids.filtered(
-                    lambda a: a.company_id == self.company_id
-                )
+                domain = [
+                    ("code", ">=", start_range),
+                    ("code", "<=", end_range),
+                    ("company_ids", "in", self.company_id.id),
+                ]
+            else:
+                domain = [
+                    ("code", ">=", start_range),
+                    ("code", "<=", end_range),
+                ]
+            self.account_ids = self.env["account.account"].search(domain)
 
     def _init_date_from(self):
         """set start date to begin of current year if fiscal year running"""
